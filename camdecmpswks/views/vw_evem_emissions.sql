15e98f340e3abd2758a334c6fd3e32261ba49edc
Jason Whitehead <jwhitehead@austin.rr.com>
2021-10-07 16:01:58 -0500

    few more conversions and renames

---
+++ camdecmpswks/views/vw_evem_emissions.sql
@@ -0,0 +1,45 @@
+-- View: camdecmpswks.vw_evem_emissions
+
+-- DROP VIEW camdecmpswks.vw_evem_emissions;
+
+CREATE OR REPLACE VIEW camdecmpswks.vw_evem_emissions
+ AS
+ SELECT ee.mon_plan_id,
+    ee.rpt_period_id,
+    rp.calendar_year,
+    rp.quarter,
+        CASE
+            WHEN ee.updated_status_flg IS NULL THEN 'Y'::character varying
+            ELSE ee.updated_status_flg
+        END AS updated_status_flg,
+        CASE
+            WHEN ee.needs_eval_flg IS NULL THEN 'Y'::character varying
+            ELSE ee.needs_eval_flg
+        END AS needs_eval_flg,
+    cs.severity_cd,
+        CASE
+            WHEN esa.sub_availability_cd::text = ANY (ARRAY['GRANTED'::character varying, 'REQUIRE'::character varying]::text[]) THEN 'Y'::text
+            ELSE 'N'::text
+        END AS can_submit,
+    esa.sub_availability_cd AS submission_availability_cd
+   FROM camdecmps.emission_evaluation ee
+     JOIN camdecmpsmd.reporting_period rp ON ee.rpt_period_id = rp.rpt_period_id
+     LEFT JOIN camdecmpswks.check_session cs ON ee.chk_session_id::text = cs.chk_session_id::text
+     LEFT JOIN camdecmpsaux.em_submission_access esa ON ee.mon_plan_id::text = esa.mon_plan_id::text AND ee.rpt_period_id = esa.rpt_period_id
+UNION
+ SELECT esa.mon_plan_id,
+    esa.rpt_period_id,
+    rp.calendar_year,
+    rp.quarter,
+    'NODATA'::text AS updated_status_flg,
+    NULL::character varying AS needs_eval_flg,
+    NULL::character varying AS severity_cd,
+        CASE
+            WHEN esa.sub_availability_cd::text = ANY (ARRAY['GRANTED'::character varying, 'REQUIRE'::character varying]::text[]) THEN 'Y'::text
+            ELSE 'N'::text
+        END AS can_submit,
+    esa.sub_availability_cd AS submission_availability_cd
+   FROM camdecmpsaux.em_submission_access esa
+     JOIN camdecmpsmd.reporting_period rp ON esa.rpt_period_id = rp.rpt_period_id
+     LEFT JOIN camdecmps.emission_evaluation ee ON esa.mon_plan_id::text = ee.mon_plan_id::text AND esa.rpt_period_id = ee.rpt_period_id
+  WHERE ee.mon_plan_id IS NULL;
---
+++ camdecmpswks/views/vw_qa_test_summary.sql
@@ -0,0 +1,64 @@
+-- View: camdecmpswks.vw_qa_test_summary
+
+-- DROP VIEW camdecmpswks.vw_qa_test_summary;
+
+CREATE OR REPLACE VIEW camdecmpswks.vw_qa_test_summary
+ AS
+ SELECT ts.test_sum_id,
+    ts.mon_loc_id,
+    ts.mon_sys_id,
+    ts.component_id,
+    ts.test_num,
+    ts.gp_ind,
+    ts.test_type_cd,
+    ts.test_reason_cd,
+    ts.test_result_cd,
+    ts.calc_test_result_cd,
+    rp.calendar_year,
+    rp.quarter,
+    ts.test_description,
+    ts.rpt_period_id,
+    ts.begin_date,
+    ts.begin_hour,
+    ts.begin_min,
+    ts.end_date,
+    ts.end_hour,
+    ts.end_min,
+    ts.span_scale_cd,
+    ts.test_comment,
+    ts.injection_protocol_cd,
+    ts.last_updated,
+    ts.updated_status_flg,
+    ts.needs_eval_flg,
+    ts.chk_session_id,
+    ts.userid,
+    ts.add_date,
+    ts.update_date,
+    ml.fac_id,
+    COALESCE(ml.stack_name, ml.unitid) AS location_identifier,
+    c.component_type_cd,
+    c.component_identifier,
+    c.acq_cd,
+    ms.system_identifier,
+    ms.sys_type_cd,
+    ms.sys_designation_cd,
+    ff.reinstall_date,
+    ff.reinstall_hour,
+    udt.fuel_cd,
+    fc.fuel_group_cd,
+    fc.unit_fuel_cd,
+    fc.fuel_cd_description,
+    udt.operating_condition_cd,
+    occ.op_condition_cd_description,
+    COALESCE(flc.op_level_cd, flr.op_level_cd) AS op_level_cd
+   FROM camdecmps.test_summary ts
+     LEFT JOIN camdecmpswks.vw_monitor_location ml ON ts.mon_loc_id::text = ml.mon_loc_id::text
+     LEFT JOIN camdecmpswks.monitor_system ms ON ts.mon_sys_id::text = ms.mon_sys_id::text
+     LEFT JOIN camdecmpswks.component c ON ts.component_id::text = c.component_id::text
+     LEFT JOIN camdecmpsmd.reporting_period rp ON ts.rpt_period_id = rp.rpt_period_id
+     LEFT JOIN camdecmps.fuel_flowmeter_accuracy ff ON ts.test_sum_id::text = ff.test_sum_id::text
+     LEFT JOIN camdecmps.unit_default_test udt ON ts.test_sum_id::text = udt.test_sum_id::text
+     LEFT JOIN camdecmpsmd.fuel_code fc ON udt.fuel_cd::text = fc.fuel_cd::text
+     LEFT JOIN camdecmpsmd.operating_condition_code occ ON udt.operating_condition_cd::text = occ.operating_condition_cd::text
+     LEFT JOIN camdecmps.flow_to_load_check flc ON ts.test_sum_id::text = flc.test_sum_id::text
+     LEFT JOIN camdecmps.flow_to_load_reference flr ON ts.test_sum_id::text = flr.test_sum_id::text;
