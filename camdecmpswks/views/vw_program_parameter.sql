cd4409d3ed91e43c49f55f0c21112f1106d2c8f1
Dwayne Whitten <Dwayne.Whitten@erg.com>
2021-10-08 09:36:21 -0400

    Addition of vw_unit_program_exemption and updated format for vw_program_parameter

--- camdecmpswks/views/vw_program_parameter.sql
+++ camdecmpswks/views/vw_program_parameter.sql
@@ -1,14 +1,14 @@
-CREATE OR REPLACE VIEW camdecmpswks.vw_program_parameter AS 
-SELECT	pp.prg_param_id ,
-		pp.prg_id ,
-		pp.parameter_cd ,
-		pp.required_ind ,
-		pc.os_ind ,
-		pp.begin_rpt_period_id ,
-		pp.end_rpt_period_id ,
-		pp.userid ,
-		pp.add_date ,
-		pp.update_date 
-  FROM	CAMDECMPSAUX.program_parameter pp 
-  		JOIN CAMD."program" p on p.prg_id = pp.prg_id 
-  		JOIN CAMDMD.program_code pc on pc.prg_cd  = p.prg_cd 
+CREATE OR REPLACE VIEW camdecmpswks.vw_program_parameter
+AS SELECT pp.prg_param_id,
+    pp.prg_id,
+    pp.parameter_cd,
+    pp.required_ind,
+    pc.os_ind,
+    pp.begin_rpt_period_id,
+    pp.end_rpt_period_id,
+    pp.userid,
+    pp.add_date,
+    pp.update_date
+   FROM camdecmpsaux.program_parameter pp
+     JOIN camd.program p ON p.prg_id = pp.prg_id
+     JOIN camdmd.program_code pc ON pc.prg_cd::text = p.prg_cd::text;
