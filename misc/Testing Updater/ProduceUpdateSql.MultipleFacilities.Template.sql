b0c3f69316cdad893aab4cb9270bf2d4888258fa
Dwayne Whitten <Dwayne.Whitten@erg.com>
2021-10-18 09:56:21 -0400

    Updated Test Scripting to use ORIS codes instead Facility Ids.

--- misc/Testing Updater/ProduceUpdateSql.MultipleFacilities.Template.sql
+++ misc/Testing Updater/ProduceUpdateSql.MultipleFacilities.Template.sql
@@ -2,12 +2,12 @@ USE [EASEY-IN]
 GO
 
 /*
-    1) Modify and run the script.
-    2) Say the results as a SQL script by right clicking on the results grid using "Save Result As", changing "Save as Type" to "All Files", and using a file name with a ".sql" extension.
+    1) Replace the ORIS_CODE and run the script.
+    2) Save the results as a SQL script by right clicking on the results grid using "Save Result As", changing "Save as Type" to "All Files", and using a file name with a ".sql" extension.
 */
 
-select * from [SqlGen].[PgMpInserts] ( 1 /* Replace FAC_ID */ ) union all
+select * from [SqlGen].[PgMpInserts] ( 3 /* Replace ORIS_CODE */ ) union all
 /* Repeat Above */
-select * from [SqlGen].[PgMpInserts] ( 8237 /* Replace FAC_ID */ )
+select * from [SqlGen].[PgMpInserts] ( 9 /* Replace ORIS_CODE */ )
 order by 1
 GO
--- misc/Testing Updater/ProduceUpdateSql.SingleFacility.Template.sql
+++ misc/Testing Updater/ProduceUpdateSql.SingleFacility.Template.sql
@@ -2,12 +2,12 @@ USE [EASEY-IN]
 GO
 
 /*
-    1) Modify and run the script.
-    2) Say the results as a SQL script by right clicking on the results grid using "Save Result As", changing "Save as Type" to "All Files", and using a file name with a ".sql" extension.
+    1) Replace the ORIS_CODE and run the script.
+    2) Save the results as a SQL script by right clicking on the results grid using "Save Result As", changing "Save as Type" to "All Files", and using a file name with a ".sql" extension.
 */
 
 select  *
-  from  [SqlGen].[PgMpInserts] ( 8237 /* Replace FAC_ID */ ) 
+  from  [SqlGen].[PgMpInserts] ( 9 /* Replace ORIS_CODE */ ) 
  order
     by  1
 GO
