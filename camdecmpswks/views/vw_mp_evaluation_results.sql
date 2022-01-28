a9135e39a89e5e3217aa26cef8a14752736aac29
Conrad Sanley <csanley@cvpcorp.com>
2022-01-28 14:03:31 -0500

    Updated View details to address displaying MONPLAN checks (which don't have a mon_loc_id)

--- camdecmpswks/views/vw_mp_evaluation_results.sql
+++ camdecmpswks/views/vw_mp_evaluation_results.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE VIEW camdecmpswks.vw_mp_evaluation_results
         CASE
             WHEN ml.stack_pipe_id IS NOT NULL THEN sp.stack_name
             WHEN ml.unit_id IS NOT NULL then u.unitid
+			WHEN cl.mon_loc_id IS NULL then 'All Locations'
 			ELSE 'Undetermined'
         END AS unitstackinformation,
     cl.severity_cd AS severitycode,
@@ -15,14 +16,14 @@ CREATE OR REPLACE VIEW camdecmpswks.vw_mp_evaluation_results
     (((cc.check_type_cd::text || '-'::text) || cc.check_number) || '-'::text) || ccr.check_result::text AS checkcode,
     cl.result_message AS resultmessage
    FROM camdecmpswks.check_log cl
-     JOIN camdecmpswks.monitor_location ml ON cl.mon_loc_id::text = ml.mon_loc_id::text
+     JOIN camdecmpswks.check_session cs ON cl.chk_session_id::text = cs.chk_session_id::text
+	 LEFT JOIN camdecmpswks.monitor_location ml ON cl.mon_loc_id::text = ml.mon_loc_id::text
      LEFT JOIN camdecmpswks.stack_pipe sp ON ml.stack_pipe_id::text = sp.stack_pipe_id::text
      LEFT JOIN camd.unit u ON ml.unit_id = u.unit_id
      JOIN camdecmpsmd.check_catalog_result ccr ON cl.check_catalog_result_id = ccr.check_catalog_result_id
      JOIN camdecmpsmd.check_catalog cc ON ccr.check_catalog_id = cc.check_catalog_id
      JOIN camdecmpsmd.rule_check rc ON cc.check_catalog_id = rc.check_catalog_id
-     JOIN camdecmpsmd.category_code ccd ON rc.category_cd::text = ccd.category_cd::text AND ccd.process_cd::text = 'MP'::text
-     JOIN camdecmpswks.check_session cs ON cl.chk_session_id::text = cs.chk_session_id::text;
+     JOIN camdecmpsmd.category_code ccd ON rc.category_cd::text = ccd.category_cd::text AND ccd.process_cd::text = 'MP'::text;
 
 ALTER TABLE camdecmpswks.vw_mp_evaluation_results
     OWNER TO "uImcwuf4K9dyaxeL";
