ebf85325dd0a788edb70c4a09e89b9500a8868c0
Conrad Sanley <csanley@cvpcorp.com>
2022-02-17 17:01:25 -0500

    updated reference from parameter_code to controlEquipParamCode per ticket 2628

--- camdecmpsmd/views/vw_unitcontrol_master_data_relationships.sql
+++ camdecmpsmd/views/vw_unitcontrol_master_data_relationships.sql
@@ -4,15 +4,15 @@
 
 CREATE OR REPLACE VIEW camdecmpsmd.vw_unitcontrol_master_data_relationships
  AS
- SELECT unit_control.parameter_code,
+ SELECT unit_control.controlEquipParamCode,
     control_code.control_code
-   FROM ( SELECT param.value1 AS parameter_code
+   FROM ( SELECT param.value1 AS controlEquipParamCode
            FROM camdecmpsmd.vw_cross_check_catalog_value param
           WHERE param.cross_chk_catalog_name::text = 'Parameter to Category'::text AND param.value2 = 'UNITCONTROL'::text) unit_control
-     LEFT JOIN ( SELECT DISTINCT vw_cross_check_catalog_value.value1 AS parameter_code,
+     LEFT JOIN ( SELECT DISTINCT vw_cross_check_catalog_value.value1 AS controlEquipParamCode,
             vw_cross_check_catalog_value.value2 AS control_code
            FROM camdecmpsmd.vw_cross_check_catalog_value
-          WHERE vw_cross_check_catalog_value.cross_chk_catalog_name::text = 'Parameter Code to Control Code for Unit Control'::text) control_code ON unit_control.parameter_code = control_code.parameter_code;
+          WHERE vw_cross_check_catalog_value.cross_chk_catalog_name::text = 'Parameter Code to Control Code for Unit Control'::text) control_code ON unit_control.controlEquipParamCode = control_code.controlEquipParamCode;
 
 ALTER TABLE camdecmpsmd.vw_unitcontrol_master_data_relationships
     OWNER TO "uImcwuf4K9dyaxeL";
