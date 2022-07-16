820de87358973335b5596fc87702406f8690df98
Jason Whitehead <jwhitehead@austin.rr.com>
2022-07-15 23:32:49 -0500

    added test type group codes and test summary mdm relationships

--- camdecmpswks/2-after-load/Add_FK_delete_constraint_CheckLog_table.sql
+++ camdecmpswks/2-after-load/Add_FK_delete_constraint_CheckLog_table.sql
@@ -1,9 +1,9 @@
--- ADD FK CONSTRAINT FOR CHECK ENGINE
+-- Constraint: fk_check_log_check_session
+
+-- ALTER TABLE camdecmpswks.check_log DROP CONSTRAINT fk_check_log_check_session;
+
 ALTER TABLE camdecmpswks.check_log
-    DROP CONSTRAINT pk_check_session_check_log,
-	ADD CONSTRAINT pk_check_session_check_log FOREIGN KEY (chk_session_id)
+    ADD CONSTRAINT fk_check_log_check_session FOREIGN KEY (chk_session_id)
     REFERENCES camdecmpswks.check_session (chk_session_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE CASCADE;
-	
-
