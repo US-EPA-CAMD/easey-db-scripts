eb29090aea3fe1253f47960443b34c0e41e36cd5
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-10-18 15:02:46 -0400

    hiunitstack conversion complete

---
+++ camdecmps/tables/10-emission-view-hiunitstack.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS camdecmps.emission_view_hiunitstack(
+    em_hi_unitstack_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    mon_plan_id VARCHAR(45) NOT NULL,
+    mon_loc_id VARCHAR(45) NOT NULL,
+    rpt_period_id INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL,
+    hour NUMERIC(2,0) NOT NULL,
+    op_time NUMERIC(3,2),
+    unit_load NUMERIC(6,0),
+    load_uom VARCHAR(7),
+    load_range INTEGER,
+    common_stack_load_range INTEGER,
+    hi_formula_code VARCHAR(7),
+    rpt_hi_rate NUMERIC(14,4),
+    calc_hi_rate NUMERIC(14,4),
+    error_codes VARCHAR(1000)
+)
