a721b79f1b89961dd4400d13bbe4f1159820abbf
Jason Whitehead <jwhitehead@austin.rr.com>
2022-11-01 16:42:33 -0500

    added certification statements table and data load

--- camddmw/views/vw_rep_display_fact.sql
+++ camddmw/views/vw_rep_display_fact.sql
@@ -13,11 +13,12 @@ CREATE OR REPLACE VIEW camddmw.vw_rep_display_fact
                     WHEN rep_year_dim.op_year::double precision = date_part('year'::text, rep_year_dim.begin_date) AND rep_year_dim.op_year::double precision = date_part('year'::text, rep_year_dim.end_date) THEN ((((rep_year_dim.ppl_id::text || ' (Started '::text) || to_char(rep_year_dim.begin_date, 'Mon dd, YYYY'::text)) || ')(Ended '::text) || to_char(rep_year_dim.end_date, 'Mon dd, YYYY'::text)) || ')'::text
                     WHEN rep_year_dim.op_year::double precision = date_part('year'::text, rep_year_dim.end_date) THEN ((rep_year_dim.ppl_id::text || ' (Ended '::text) || to_char(rep_year_dim.end_date, 'Mon dd, YYYY'::text)) || ')'::text
                     WHEN rep_year_dim.op_year::double precision = date_part('year'::text, rep_year_dim.begin_date) THEN ((rep_year_dim.ppl_id::text || ' (Started '::text) || to_char(rep_year_dim.begin_date, 'Mon dd, YYYY'::text)) || ')'::text
+                    WHEN rep_year_dim.ppl_id IS NULL THEN NULL::text
                     ELSE rep_year_dim.ppl_id::text
                 END AS rep_info,
             row_number() OVER (PARTITION BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.ppl_id ORDER BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.begin_date) AS row_num
            FROM camddmw.rep_year_dim
-          WHERE rep_year_dim.rep_code::text = 'PRM'::text
+          WHERE rep_year_dim.rep_code::text = 'PRM'::text OR rep_year_dim.rep_code::text IS NULL
           GROUP BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.ppl_id, rep_year_dim.begin_date, rep_year_dim.end_date
           ORDER BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.begin_date, (row_number() OVER (PARTITION BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.ppl_id ORDER BY rep_year_dim.unit_id, rep_year_dim.op_year, rep_year_dim.begin_date))) d
   WHERE d.row_num = 1
