387147fe03128f859fc1e032e337172f6da928f7
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-12-02 13:00:39 -0500

    Increased token length in user session

--- camdecmpswks/tables/1-user-session.sql
+++ camdecmpswks/tables/1-user-session.sql
@@ -6,7 +6,7 @@ CREATE TABLE camdecmpswks.user_session
 (
     userid character varying(25) COLLATE pg_catalog."default" NOT NULL,
     session_id character varying(45) COLLATE pg_catalog."default",
-    security_token character varying(1000) COLLATE pg_catalog."default",
+    security_token character varying(50000) COLLATE pg_catalog."default",
     token_expiration timestamp without time zone NOT NULL DEFAULT (CURRENT_TIMESTAMP + '00:20:00'::interval),
     last_login_date timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT user_session_pkey PRIMARY KEY (userid)
