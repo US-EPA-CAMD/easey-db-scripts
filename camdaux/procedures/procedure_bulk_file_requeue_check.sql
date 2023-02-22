5a19d48c81488b23bbd8de1f3544fa886471b302
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-02-22 13:11:43 -0500

    Db changes for bulk file generation, and evaluation set

--- camdaux/procedures/procedure_bulk_file_requeue_check.sql
+++ camdaux/procedures/procedure_bulk_file_requeue_check.sql
@@ -1,14 +1,10 @@
--- PROCEDURE: camdaux.procedure_bulk_file_requeue_check()
-
-DROP PROCEDURE IF EXISTS camdaux.procedure_bulk_file_requeue_check();
-
 CREATE OR REPLACE PROCEDURE camdaux.procedure_bulk_file_requeue_check(
 	)
 LANGUAGE 'sql'
 AS $BODY$
 SET timezone = 'America/New_York';
 
-update camdaux.job_log jl
+update camdaux.bulk_file_queue jl
 set status_cd = 'QUEUED'
-where jl.status_cd::text = 'ERROR'::text OR (jl.status_cd::text = 'QUEUED'::text OR jl.status_cd::text = 'WIP'::text) AND timezone('est'::text, CURRENT_TIMESTAMP) >= (jl.add_date + '24:00:00'::interval)
+where jl.status_cd::text = 'ERROR'::text OR jl.status_cd::text = 'WIP'::text AND timezone('est'::text, CURRENT_TIMESTAMP) >= (jl.start_date + '24:00:00'::interval)
 $BODY$;
--- camdecmpsaux/tables/1-evaluation_set.sql
+++ camdecmpsaux/tables/1-evaluation_set.sql
@@ -16,10 +16,6 @@ CREATE TABLE IF NOT EXISTS camdecmpsaux.evaluation_set
     CONSTRAINT fk_evaluation_set_monitor_plan FOREIGN KEY (mon_plan_id)
         REFERENCES camdecmps.monitor_plan (mon_plan_id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fk_submission_set_plant FOREIGN KEY (fac_id)
-        REFERENCES camd.plant (fac_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
 
