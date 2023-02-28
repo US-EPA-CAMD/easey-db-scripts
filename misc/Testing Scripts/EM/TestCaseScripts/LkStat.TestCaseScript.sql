482bc70fa441c2b77f68182262a7c1c6a8307394
Dwayne Whitten <Dwayne.Whitten@erg.com>
2023-02-28 13:33:10 -0500

    Emissions Evaluation Test Case Scripting: LKSTAT

---
+++ misc/Testing Scripts/EM/TestCaseScripts/LkStat.TestCaseScript.sql
@@ -0,0 +1,3 @@
+/* ORIS:   6193    Locations: 061B       Quarter: 2021 Q4           Location:           SQL Ord:    0 */ call camdecmpswks.load_emissions_workspace( 'MDC-519E245D136246FDBF31B1B2BA8AAD36', 2021, 4 );
+/* ORIS:   6193    Locations: 062B       Quarter: 2021 Q4           Location:           SQL Ord:    0 */ call camdecmpswks.load_emissions_workspace( 'MDC-95170A4EC71440E2B533BF7082194FCC', 2021, 4 );
+/* ORIS:  10671    Locations: CS1, 1A    Quarter: 2021 Q4           Location:           SQL Ord:    0 */ call camdecmpswks.load_emissions_workspace( 'ECG-0003-296AB4D76ECE49ACA9DA77494C813BBD', 2021, 4 );
