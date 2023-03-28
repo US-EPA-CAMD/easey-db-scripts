21d9f7cf5e70ee15492e0668a91c4b1ec29d27d0
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-03-28 11:03:55 -0400

    Added applicable_bias_adj to monitor_hrly table

--- camdecmpscalc/tables/tables.sql
+++ camdecmpscalc/tables/tables.sql
@@ -311,6 +311,7 @@ CREATE TABLE IF NOT EXISTS camdecmpscalc.monitor_hrly_value
     chk_session_id character varying(45) NOT NULL,
     monitor_hrly_val_id character varying(45) NOT NULL,
     calc_adjusted_hrly_value numeric(13,3),
+    applicable_bias_adj_factor numeric(5,3),
     calc_line_status character varying(75),
     calc_rata_status character varying(75),
     calc_daycal_status character varying(75),
