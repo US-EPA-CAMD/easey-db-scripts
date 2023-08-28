56cc54e84780f2ef24c3527ebdbf6870bba37626
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-08-28 16:24:03 -0400

    Change to make sub availability codes non updated returned for mats bulk file view

--- camdecmpswks/views/vw_mats_bulk_file_eval_and_submit.sql
+++ camdecmpswks/views/vw_mats_bulk_file_eval_and_submit.sql
@@ -16,11 +16,12 @@ CREATE OR REPLACE VIEW camdecmpswks.vw_mats_bulk_file_eval_and_submit
     mbf.updated_status_flg
    FROM camdecmpswks.mats_bulk_file mbf
      JOIN camdecmpswks.monitor_plan_location mpl USING (mon_plan_id)
-     JOIN camdecmpswks.monitor_location ml on  ml.mon_loc_id = mpl.mon_loc_id
+     JOIN camdecmpswks.monitor_location ml ON ml.mon_loc_id::text = mpl.mon_loc_id::text
      JOIN camdecmpsmd.submission_availability_code sac ON sac.submission_availability_cd::text = mbf.submission_availability_cd::text
-	 LEFT JOIN camdecmpswks.test_summary ts on (ts.test_num = mbf.test_num and ts.mon_loc_id = ml.mon_loc_id)
-     LEFT JOIN camdecmpswks.monitor_system ms on ms.mon_sys_id = ts.mon_sys_id
-     LEFT JOIN camdecmpswks.component c on c.component_id = ts.component_id
+     LEFT JOIN camdecmpswks.test_summary ts ON ts.test_num::text = mbf.test_num::text AND ts.mon_loc_id::text = ml.mon_loc_id::text
+     LEFT JOIN camdecmpswks.monitor_system ms ON ms.mon_sys_id::text = ts.mon_sys_id::text
+     LEFT JOIN camdecmpswks.component c ON c.component_id::text = ts.component_id::text
      LEFT JOIN camd.unit u USING (unit_id)
      LEFT JOIN camdecmps.stack_pipe sp USING (stack_pipe_id)
-  ORDER BY oris_code, mon_plan_id, u.unitid, sp.stack_name;
+	 WHERE mbf.submission_availability_cd <> 'UPDATED'
+  ORDER BY mbf.oris_code, mbf.mon_plan_id, u.unitid, sp.stack_name;
