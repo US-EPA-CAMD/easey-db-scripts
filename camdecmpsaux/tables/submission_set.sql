abcd684b464d9b1d33cbc28c38fb813c40040a8d
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-22 16:07:13 -0400

    Added submissions endpoint snippets

--- camdecmpsaux/tables/submission_set.sql
+++ camdecmpsaux/tables/submission_set.sql
@@ -1,4 +1,4 @@
-CREATE TABLE camdecmpsaux.submission_set
+CREATE TABLE IF NOT EXISTS camdecmpsaux.submission_set
 (
     submission_set_id character varying(45) COLLATE pg_catalog."default" NOT NULL,
     mon_plan_id character varying(45) COLLATE pg_catalog."default" NOT NULL,
@@ -10,5 +10,11 @@ CREATE TABLE camdecmpsaux.submission_set
     activity_id text COLLATE pg_catalog."default" NOT NULL,
     configuration character varying(100) COLLATE pg_catalog."default" NOT NULL,
     status_cd character varying(8) COLLATE pg_catalog."default",
-    details text COLLATE pg_catalog."default"
-);
+    details text COLLATE pg_catalog."default",
+    submission_end_stage_time timestamp without time zone,
+    CONSTRAINT pk_submission_set PRIMARY KEY (submission_set_id),
+    CONSTRAINT fk_submission_set_monitor_plan FOREIGN KEY (mon_plan_id)
+        REFERENCES camdecmps.monitor_plan (mon_plan_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
---
+++ camdecmpsaux/views/vw_combined_submissions.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW camdecmpsaux.vw_combined_submissions
+ AS
+ SELECT sq.submission_id,
+    sq.process_cd,
+    sq.severity_cd,
+    ss.fac_id,
+    ss.mon_plan_id,
+    sq.rpt_period_id,
+    ss.submission_set_id,
+    sq.submitted_on,
+    ss.user_id,
+    ss.submission_end_stage_time
+   FROM camdecmpsaux.submission_set ss
+     JOIN camdecmpsaux.submission_queue sq USING (submission_set_id);
