6b82268c85c8e8045b640017be643723addf2178
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-25 14:19:45 -0400

    Change oris type

--- camdecmpsaux/tables/evaluation_set.sql
+++ camdecmpsaux/tables/evaluation_set.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS camdecmpsaux.evaluation_set
     submitted_on timestamp without time zone NOT NULL,
     user_id character varying(25) COLLATE pg_catalog."default" NOT NULL,
     user_email character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    oris_code numeric(38,0) NOT NULL,
+    oris_code numeric(6,0) NOT NULL,
     fac_id numeric(38,0) NOT NULL,
     fac_name character varying(200) COLLATE pg_catalog."default" NOT NULL,
     configuration character varying(100) COLLATE pg_catalog."default" NOT NULL
--- camdecmpsaux/tables/submission_set.sql
+++ camdecmpsaux/tables/submission_set.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS camdecmpsaux.submission_set
     user_id character varying(25) COLLATE pg_catalog."default" NOT NULL,
     user_email character varying(100) COLLATE pg_catalog."default" NOT NULL,
     fac_id numeric(38,0) NOT NULL,
-    oris_code numeric NOT NULL,
+    oris_code numeric(6,0) NOT NULL,
     fac_name character varying(200) COLLATE pg_catalog."default" NOT NULL,
     activity_id text COLLATE pg_catalog."default" NOT NULL,
     configuration character varying(100) COLLATE pg_catalog."default" NOT NULL,
