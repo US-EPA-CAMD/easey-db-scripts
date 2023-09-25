ee4f746bb6f8900ed7b8b4e6d4f34d463b2a8acf
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-09-25 13:58:34 -0400

    Changes to eval + submission

--- camdecmpsaux/tables/submission_queue.sql
+++ camdecmpsaux/tables/submission_queue.sql
@@ -1,4 +1,4 @@
-CREATE TABLE camdecmpsaux.submission_queue
+CREATE TABLE IF NOT EXISTS camdecmpsaux.submission_queue
 (
     submission_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999999 CACHE 1 ),
     submission_set_id character varying(45) COLLATE pg_catalog."default" NOT NULL,
@@ -12,4 +12,4 @@ CREATE TABLE camdecmpsaux.submission_queue
     submitted_on timestamp without time zone NOT NULL,
     status_cd character varying(8) COLLATE pg_catalog."default" NOT NULL,
     details text COLLATE pg_catalog."default"
-);
+)
