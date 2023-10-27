517296765d2ae999d20c8a94e33a8c9d555a8667
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-10-27 15:23:16 -0400

    Fix to emission submission access windows being returned

--- camdecmps/views/vw_em_eval_and_submit.sql
+++ camdecmps/views/vw_em_eval_and_submit.sql
@@ -5,8 +5,8 @@ CREATE OR REPLACE VIEW camdecmps.vw_em_eval_and_submit
     d.mon_plan_id,
     d.configuration,
     esa.userid,
-    COALESCE(esa.update_date, esa.add_date) AS update_date,
-    esa.sub_availability_cd AS window_status,
+    esa.update_date AS update_date,
+    esa.window_status AS window_status,
     rpt.period_abbreviation
    FROM camd.plant p
      JOIN camdecmps.monitor_plan mp USING (fac_id)
@@ -24,5 +24,15 @@ CREATE OR REPLACE VIEW camdecmps.vw_em_eval_and_submit
 	 ) AS d USING(mon_plan_id)
      JOIN camdecmps.emission_evaluation ee USING(mon_plan_id)
      JOIN camdecmpsmd.reporting_period rpt ON ee.rpt_period_id = rpt.rpt_period_id
-     LEFT JOIN camdecmpsaux.em_submission_access esa ON ee.mon_plan_id::text = esa.mon_plan_id::text AND ee.rpt_period_id = esa.rpt_period_id AND esa.em_status_cd::text <> 'RECVD'::text
+     LEFT JOIN (
+      SELECT DISTINCT ON (esa.mon_plan_id, esa.rpt_period_id)
+        esa.mon_plan_id,
+        esa.rpt_period_id,
+        esa.userid,
+        COALESCE(esa.update_date, esa.add_date) AS update_date,
+        esa.sub_availability_cd AS window_status
+      FROM camdecmpsaux.em_submission_access esa
+      WHERE esa.em_status_cd::text <> 'RECVD'::text
+      ORDER BY esa.mon_plan_id, esa.rpt_period_id, esa.access_begin_date DESC
+	) esa ON ee.mon_plan_id::text = esa.mon_plan_id::text AND ee.rpt_period_id = esa.rpt_period_id
 	ORDER BY p.oris_code, configuration, rpt.period_abbreviation;
--- camdecmpswks/views/vw_em_eval_and_submit.sql
+++ camdecmpswks/views/vw_em_eval_and_submit.sql
@@ -13,8 +13,8 @@ CREATE OR REPLACE VIEW camdecmpswks.vw_em_eval_and_submit
     ee.submission_availability_cd,
     sac.sub_avail_cd_description AS submission_availability_cd_description,
     esa.userid,
-    COALESCE(esa.update_date, esa.add_date) AS update_date,
-    esa.sub_availability_cd AS window_status,
+    esa.update_date AS update_date,
+    esa.window_status AS window_status,
     rpt.period_abbreviation
    FROM camd.plant p
      JOIN camdecmpswks.monitor_plan mp USING (fac_id)
@@ -34,5 +34,15 @@ CREATE OR REPLACE VIEW camdecmpswks.vw_em_eval_and_submit
      JOIN camdecmpsmd.reporting_period rpt ON ee.rpt_period_id = rpt.rpt_period_id
      JOIN camdecmpsmd.eval_status_code esc ON esc.eval_status_cd::text = ee.eval_status_cd::text
      LEFT JOIN camdecmpsmd.submission_availability_code sac ON sac.submission_availability_cd::text = ee.submission_availability_cd::text
-     LEFT JOIN camdecmpsaux.em_submission_access esa ON ee.mon_plan_id::text = esa.mon_plan_id::text AND ee.rpt_period_id = esa.rpt_period_id AND esa.em_status_cd::text <> 'RECVD'::text
+     LEFT JOIN (
+      SELECT DISTINCT ON (esa.mon_plan_id, esa.rpt_period_id)
+        esa.mon_plan_id,
+        esa.rpt_period_id,
+        esa.userid,
+        COALESCE(esa.update_date, esa.add_date) AS update_date,
+        esa.sub_availability_cd AS window_status
+      FROM camdecmpsaux.em_submission_access esa
+      WHERE esa.em_status_cd::text <> 'RECVD'::text
+      ORDER BY esa.mon_plan_id, esa.rpt_period_id, esa.access_begin_date DESC
+	) esa ON ee.mon_plan_id::text = esa.mon_plan_id::text AND ee.rpt_period_id = esa.rpt_period_id
 	ORDER BY p.oris_code, configuration, rpt.period_abbreviation;
