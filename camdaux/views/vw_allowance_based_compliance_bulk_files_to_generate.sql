df1f6de86e5e1df23789c7f42515913360c23b3a
ergjustin <justin.joseph.erg.com>
2024-01-31 14:52:32 -0600

    current timestamp change to last time the specific Quartz job ran

--- camdaux/views/vw_allowance_based_compliance_bulk_files_to_generate.sql
+++ camdaux/views/vw_allowance_based_compliance_bulk_files_to_generate.sql
@@ -9,4 +9,11 @@ CREATE OR REPLACE VIEW camdaux.vw_allowance_based_compliance_bulk_files_to_gener
    FROM camddmw.account_compliance_dim acd
      JOIN camddmw.account_fact af ON acd.account_number::text = af.account_number::text AND acd.prg_code::text = af.prg_code::text
      JOIN camdmd.program_code pc ON acd.prg_code::text = pc.prg_cd::text AND pc.bulk_file_active = 1::numeric
-  WHERE (acd.last_update_date >= date(timezone('est'::text, CURRENT_TIMESTAMP) - '1 day'::interval) OR af.last_update_date >= date(timezone('est'::text, CURRENT_TIMESTAMP) - '1 day'::interval)) AND camdaux.can_generate_compliance(acd.op_year::integer, acd.prg_code);
+      JOIN (
+              SELECT  coalesce(max(job.Start_Date), current_timestamp) AS Last_Start_Timestamp
+                 FROM  JOB_LOG job
+                 WHERE  job.JOB_SYSTEM = 'Quartz' AND job.JOB_CLASS = 'Bulk Data File' AND job.JOB_NAME = 'Allowance Compliance'
+           ) cmp
+          ON null is null
+  WHERE  (acd.last_update_date >= date(timezone('est'::text, cmp.Last_Start_Timestamp) - '1 day'::interval) OR af.last_update_date >= date(timezone('est'::text, cmp.Last_Start_Timestamp) - '1 day'::interval)) AND camdaux.can_generate_compliance(acd.op_year::integer, acd.prg_code)
+  GROUP BY  acd.prg_code, acd.op_year;
