f5243d4c213e2008cc5e68715db33846a64dee69
Mark Hayward <mark.hayward@erg.com>
2024-05-03 16:30:52 -0400

    update to use commas (instead of semicolons) to separate each new column

--- deployments/ecmps/release-v2.0-fix/Sprint4/6206/camdecmpswks/user_session.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint4/6206/camdecmpswks/user_session.sql
@@ -1,5 +1,5 @@
 ALTER TABLE camdecmpswks.user_session
-ADD COLUMN oidc_policy character varying(160) COLLATE pg_catalog."default";
-ADD COLUMN client_ip character varying(160) COLLATE pg_catalog."default";
-ADD COLUMN refresh_token text COLLATE pg_catalog."default";
+ADD COLUMN oidc_policy character varying(160) COLLATE pg_catalog."default",
+ADD COLUMN client_ip character varying(160) COLLATE pg_catalog."default",
+ADD COLUMN refresh_token text COLLATE pg_catalog."default",
 ADD COLUMN roles text COLLATE  pg_catalog."default";
