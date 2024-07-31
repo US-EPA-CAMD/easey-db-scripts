da64cfb6dc20006e93c1fffdca7b02847dd5817c
Jon Maxwell Diebold <max.diebold@erg.com>
2024-07-31 12:32:55 -0400

    revert(5995): removed unused files

--- camdecmpswks/constraints-indexes/2-user_check_out_new.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE IF EXISTS camdecmpswks.user_check_out_new
-    ADD CONSTRAINT pk_user_check_out_new PRIMARY KEY (facility_id);
-
--- camdecmpswks/tables/user_check_out_new.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS camdecmpswks.user_check_out_new (
-    facility_id integer NOT NULL,
-    checked_out_on timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    checked_out_by text COLLATE pg_catalog."default" NOT NULL,
-    last_activity timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
