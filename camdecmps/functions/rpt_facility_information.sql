f94d60b2c35346d54ba3198a3e5d0c6727a519e4
ergjustin <justin.joseph.erg.com>
2024-08-02 18:33:49 -0500

    fix:update facility informations function

--- camdecmps/functions/rpt_facility_information.sql
+++ camdecmps/functions/rpt_facility_information.sql
@@ -88,28 +88,31 @@ CREATE OR REPLACE FUNCTION camdecmps.rpt_facility_information(
     ROWS 1000
     
 AS $BODY$
-SELECT
-		p.facility_name AS "facilityName",
-		p.oris_code AS "orisCode",
-		(
-			SELECT
-				string_agg(coalesce(unitid, stack_name), ', ')
-			FROM (
-				SELECT
-					u.unitid,
-					sp.stack_name
-				FROM camdecmps.monitor_plan_location mpl
-				JOIN camdecmps.monitor_location ml USING(mon_loc_id)
-				LEFT JOIN camdecmps.stack_pipe sp USING(stack_pipe_id)
-				LEFT JOIN camd.unit u USING(unit_id)
-				WHERE mon_plan_id = vmonplanid
-				GROUP BY u.unitid, sp.stack_name
-				ORDER BY u.unitid, sp.stack_name
-			) d
-		) AS "locationInfo",
-		p.state AS "stateCode",
-		cc.county_name AS "countyName"
-	FROM camd.plant p
-	JOIN camdmd.county_code cc USING(county_cd)
-	WHERE p.oris_code = vfacilityid 
+SELECT 
+  p.facility_name AS "facilityName", 
+  p.oris_code AS "orisCode", 
+  (
+    SELECT 
+      string_agg(
+        coalesce(u.unitid, sp.stack_name), 
+        ', ' 
+        ORDER BY 
+          u.unitid, 
+          sp.stack_name
+      ) 
+    FROM 
+      camdecmps.monitor_plan_location mpl 
+      JOIN camdecmps.monitor_location ml USING(mon_loc_id) 
+      LEFT JOIN camdecmps.stack_pipe sp USING(stack_pipe_id) 
+      LEFT JOIN camd.unit u USING(unit_id) 
+    WHERE 
+      mpl.mon_plan_id = vmonplanid
+  ) AS "locationInfo", 
+  p.state AS "stateCode", 
+  cc.county_name AS "countyName" 
+FROM 
+  camd.plant p 
+  JOIN camdmd.county_code cc USING(county_cd) 
+WHERE 
+  p.oris_code = vfacilityid
 $BODY$;
