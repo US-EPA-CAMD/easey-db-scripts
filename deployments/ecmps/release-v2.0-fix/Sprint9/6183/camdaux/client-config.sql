1a1752d088bc6d7f7e9ba6f7aee5755a02bf52bd
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-08-23 14:40:41 -0400

    Ticket 6183. camd-services now makes a call to CBS recipient list api to get the list of email recipients that should receive a submission email. For that, it needs a client id, secret etc. for client token generation and validation.

--- deployments/ecmps/release-v2.0-fix/Sprint9/6183/camdaux/client-config.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint9/6183/camdaux/client-config.sql
@@ -1,3 +1,3 @@
 INSERT INTO camdaux.client_config
 (client_id, client_name, client_secret, client_passcode, encryption_key, support_email)
-VALUES('75KcqRlPMBlkooyg2YwE0RO7Ihi2lh2vqVZnTny4ThU8G', 'campd-services', 'BZAOBL5bq5g9rAKy0eBYe9i2vEll8XfmedsQw1oCa5pdV', 'At5LF3Hv3ChPXFboxocF9GzzbqFu3ZVmsb22zeMMBge1Y', 'pkzehD4pisZGgcwkq8mIqreUkyTtwfnUVKQLQAibGeo9x', null);
+VALUES('', 'campd-services', '', '', '', null);
