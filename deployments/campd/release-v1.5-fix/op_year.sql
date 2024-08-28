2a4075df1e4f6623eedfa234763ca72b6439f542
Anna <anna.yang@erg.com>
2024-08-28 17:03:37 -0400

    Delete deployments/campd/release-v1.5-fix directory

--- deployments/campd/release-v1.5-fix/op_year.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS camddmw.op_year
-(
-  op_year  numeric(4,0) NOT NULL,
-  archive_ind  numeric(1,0) NOT NULL default 0,
-  hourly_data_ind numeric(1,0) NOT NULL default 1,
-CONSTRAINT pk_op_year PRIMARY KEY (op_year)
-);
-COMMENT ON COLUMN camddmw.op_year.archive_ind IS '0 for false,1 for true ';
-COMMENT ON COLUMN camddmw.op_year.hourly_data_ind IS '0 for false,1 for true ';
