30dada09fcac5e1682607d72b57662ec1fb0eaaa
Anna <anna.yang@erg.com>
2024-09-12 12:44:55 -0400

    Update op_year.sql

--- deployments/campd/release-v1.5-fix/6337/op_year.sql
+++ deployments/campd/release-v1.5-fix/6337/op_year.sql
@@ -5,5 +5,7 @@ CREATE TABLE IF NOT EXISTS camddmw.op_year
   hourly_data_ind numeric(1,0) NOT NULL default 1,
 CONSTRAINT pk_op_year PRIMARY KEY (op_year)
 );
+
+COMMENT ON COLUMN camddmw.op_year.op_year IS 'Year in which data was collected';
 COMMENT ON COLUMN camddmw.op_year.archive_ind IS '0 for false,1 for true ';
 COMMENT ON COLUMN camddmw.op_year.hourly_data_ind IS '0 for false,1 for true ';
