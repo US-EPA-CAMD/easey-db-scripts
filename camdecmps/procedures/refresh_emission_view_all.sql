2e3900307df5e2fc2fa83fc101065579d6fa4227
Dwayne Whitten <Dwayne.Whitten@erg.com>
2024-10-02 15:00:35 -0400

    #6410 Emission View SP ALL and CO2CEMS Changes

--- camdecmps/procedures/refresh_emission_view_all.sql
+++ camdecmps/procedures/refresh_emission_view_all.sql
@@ -1,189 +1,124 @@
 CREATE OR REPLACE PROCEDURE camdecmps.refresh_emission_view_all(IN vmonplanid character varying, IN vrptperiodid numeric)
  LANGUAGE plpgsql
 AS $procedure$
-DECLARE
-  mhvParamCodes text[] := ARRAY['FLOW'];
-  dhvParamCodes text[] := ARRAY['HI','SO2','NOXR','NOX', 'CO2'];
-BEGIN
-  RAISE NOTICE 'Loading temp_hourly_test_errors...';
-	CALL camdecmps.load_temp_hourly_test_errors(vMonPlanId, vRptPeriodId);
-
-  RAISE NOTICE 'Deleting existing records...';
-	DELETE FROM camdecmps.EMISSION_VIEW_ALL
-	WHERE MON_PLAN_ID = vMonPlanId AND RPT_PERIOD_ID = vRptPeriodId;
-
-  RAISE NOTICE 'Refreshing view data...';
-	INSERT INTO camdecmps.EMISSION_VIEW_ALL(
-		MON_PLAN_ID,
-		MON_LOC_ID,
-		RPT_PERIOD_ID,
-		HOUR_ID,
-		DATE_HOUR,
-		OP_TIME,
-		UNIT_LOAD,
-		LOAD_UOM,
-		HI_FORMULA_cd,
-		RPT_HI_RATE,
-		CALC_HI_RATE,
-		SO2_FORMULA_cd,
-		RPT_SO2_MASS_RATE,
-		CALC_SO2_MASS_RATE,
-		nox_rate_formula_cd,
-		RPT_ADJ_NOX_RATE,
-		CALC_ADJ_NOX_RATE,
-		nox_mass_formula_cd,
-		RPT_NOX_MASS,
-		CALC_NOX_MASS,
-		CO2_FORMULA_cd,
-		RPT_CO2_MASS_RATE,
-		CALC_CO2_MASS_RATE,
-		ADJ_FLOW_USED,
-		RPT_ADJ_FLOW,
-		UNADJ_FLOW,
-		ERROR_CODES
-	)
-	SELECT
-		hod.MON_PLAN_ID, 
-		hod.MON_LOC_ID, 
-		hod.RPT_PERIOD_ID,
-		hod.HOUR_ID,
-		camdecmps.format_date_hour(hod.BEGIN_DATE, hod.BEGIN_HOUR, null) AS DATE_HOUR,
-		hod.OP_TIME,
-		hod.HR_LOAD,
-		hod.LOAD_UOM_CD,
-		mf_hi.EQUATION_CD,
-		dhv.HI_ADJUSTED_HRLY_VALUE,
-		dhv.HI_CALC_ADJUSTED_HRLY_VALUE,
-		mf_SO2.EQUATION_CD,
-		dhv.SO2_ADJUSTED_HRLY_VALUE,
-		dhv.SO2_CALC_ADJUSTED_HRLY_VALUE,
-		mf_NOXR.EQUATION_CD,
-		dhv.NOXR_ADJUSTED_HRLY_VALUE,
-		dhv.NOXR_CALC_ADJUSTED_HRLY_VALUE,
-		mf_NOX.EQUATION_CD,
-		dhv.NOX_ADJUSTED_HRLY_VALUE,
-		dhv.NOX_CALC_ADJUSTED_HRLY_VALUE,
-		mf_CO2.EQUATION_CD,
-		dhv.CO2_ADJUSTED_HRLY_VALUE,
-		dhv.CO2_CALC_ADJUSTED_HRLY_VALUE,
-		mhv.FLOW_CALC_ADJUSTED_HRLY_VALUE AS ADJ_FLOW_USED,
-		mhv.FLOW_ADJUSTED_HRLY_VALUE AS RPT_ADJ_FLOW, 
-		mhv.FLOW_UNADJUSTED_HRLY_VALUE AS UNADJ_FLOW, 
-		hod.ERROR_CODES
-	FROM temp_hourly_test_errors AS hod
-	LEFT JOIN camdecmps.get_derived_hourly_values_pivoted(
-			vmonplanid, vrptperiodid, dhvParamCodes
-		) AS dhv (
-			hour_id character varying,
-			mon_loc_id character varying,
-			rpt_period_id numeric,
-			hi_hour_id character varying,
-			hi_mon_sys_id character varying,
-			hi_mon_form_id character varying,
-			hi_adjusted_hrly_value numeric,
-			hi_calc_adjusted_hrly_value numeric,
-			hi_unadjusted_hrly_value numeric,
-			hi_calc_unadjusted_hrly_value numeric,
-			hi_applicable_bias_adj_factor numeric,
-			hi_calc_pct_moisture numeric,
-			hi_calc_pct_diluent numeric,
-			hi_pct_available numeric,
-			hi_segment_num numeric,
-			hi_operating_condition_cd character varying,
-			hi_fuel_cd character varying,
-			hi_modc_cd character varying,
-			so2_hour_id character varying,
-			so2_mon_sys_id character varying,
-			so2_mon_form_id character varying,
-			so2_adjusted_hrly_value numeric,
-			so2_calc_adjusted_hrly_value numeric,
-			so2_unadjusted_hrly_value numeric,
-			so2_calc_unadjusted_hrly_value numeric,
-			so2_applicable_bias_adj_factor numeric,
-			so2_calc_pct_moisture numeric,
-			so2_calc_pct_diluent numeric,
-			so2_pct_available numeric,
-			so2_segment_num numeric,
-			so2_operating_condition_cd character varying,
-			so2_fuel_cd character varying,
-			so2_modc_cd character varying,
-			noxr_hour_id character varying,
-			noxr_mon_sys_id character varying,
-			noxr_mon_form_id character varying,
-			noxr_adjusted_hrly_value numeric,
-			noxr_calc_adjusted_hrly_value numeric,
-			noxr_unadjusted_hrly_value numeric,
-			noxr_calc_unadjusted_hrly_value numeric,
-			noxr_applicable_bias_adj_factor numeric,
-			noxr_calc_pct_moisture numeric,
-			noxr_calc_pct_diluent numeric,
-			noxr_pct_available numeric,
-			noxr_segment_num numeric,
-			noxr_operating_condition_cd character varying,
-			noxr_fuel_cd character varying,
-			noxr_modc_cd character varying,
-			nox_hour_id character varying,
-			nox_mon_sys_id character varying,
-			nox_mon_form_id character varying,
-			nox_adjusted_hrly_value numeric,
-			nox_calc_adjusted_hrly_value numeric,
-			nox_unadjusted_hrly_value numeric,
-			nox_calc_unadjusted_hrly_value numeric,
-			nox_applicable_bias_adj_factor numeric,
-			nox_calc_pct_moisture numeric,
-			nox_calc_pct_diluent numeric,
-			nox_pct_available numeric,
-			nox_segment_num numeric,
-			nox_operating_condition_cd character varying,
-			nox_fuel_cd character varying,
-			nox_modc_cd character varying,
-			co2_hour_id character varying,
-			co2_mon_sys_id character varying,
-			co2_mon_form_id character varying,
-			co2_adjusted_hrly_value numeric,
-			co2_calc_adjusted_hrly_value numeric,
-			co2_unadjusted_hrly_value numeric,
-			co2_calc_unadjusted_hrly_value numeric,
-			co2_applicable_bias_adj_factor numeric,
-			co2_calc_pct_moisture numeric,
-			co2_calc_pct_diluent numeric,
-			co2_pct_available numeric,
-			co2_segment_num numeric,
-			co2_operating_condition_cd character varying,
-			co2_fuel_cd character varying,
-			co2_modc_cd character varying
-	) ON dhv.HOUR_ID = hod.HOUR_ID
-		AND dhv.MON_LOC_ID = hod.MON_LOC_ID
-		AND dhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
-	left JOIN camdecmps.get_monitor_hourly_values_pivoted(
-			vmonplanid, vrptperiodid, mhvParamCodes) AS mhv (
-		hour_id character varying,
-		mon_loc_id character varying,
-		rpt_period_id numeric,
-		flow_hour_id character varying,
-		flow_adjusted_hrly_value numeric,
-		flow_calc_adjusted_hrly_value numeric,
-		flow_unadjusted_hrly_value numeric,
-		flow_applicable_bias_adj_factor numeric,
-		flow_pct_available numeric,
-		flow_moisture_basis character varying,
-		flow_modc_cd character varying
-	)	ON mhv.HOUR_ID = hod.HOUR_ID
-		AND mhv.MON_LOC_ID = hod.MON_LOC_ID
-		AND mhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
-		AND mhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA mf_HI
-		ON mf_HI.MON_FORM_ID = dhv.HI_MON_FORM_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA mf_SO2
-		ON mf_SO2.MON_FORM_ID = dhv.SO2_MON_FORM_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA mf_NOXR
-		ON mf_NOXR.MON_FORM_ID = dhv.NOXR_MON_FORM_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA mf_NOX
-		ON mf_NOX.MON_FORM_ID = dhv.NOX_MON_FORM_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA mf_CO2
-		ON mf_CO2.MON_FORM_ID = dhv.CO2_MON_FORM_ID
-	where dhv.hi_hour_id is not null or dhv.so2_hour_id is not null or dhv.noxr_hour_id is not null or dhv.nox_hour_id is not null or dhv.co2_hour_id is not null
-	or mhv.flow_hour_id is not null;
+DECLAREBEGIN
+    RAISE NOTICE 'Deleting existing records...';
+	DELETE FROM camdecmps.EMISSION_VIEW_ALL	WHERE MON_PLAN_ID = vMonPlanId AND RPT_PERIOD_ID = vRptPeriodId;
+    RAISE NOTICE 'Refreshing view data...';
+	INSERT INTO camdecmps.EMISSION_VIEW_ALL(		MON_PLAN_ID,		MON_LOC_ID,		RPT_PERIOD_ID,		HOUR_ID,		DATE_HOUR,		OP_TIME,		UNIT_LOAD,		LOAD_UOM,		HI_FORMULA_cd,		RPT_HI_RATE,		CALC_HI_RATE,		SO2_FORMULA_cd,		RPT_SO2_MASS_RATE,		CALC_SO2_MASS_RATE,		nox_rate_formula_cd,		RPT_ADJ_NOX_RATE,		CALC_ADJ_NOX_RATE,		nox_mass_formula_cd,		RPT_NOX_MASS,		CALC_NOX_MASS,		CO2_FORMULA_cd,		RPT_CO2_MASS_RATE,		CALC_CO2_MASS_RATE,		ADJ_FLOW_USED,		RPT_ADJ_FLOW,		UNADJ_FLOW,		ERROR_CODES	)    select  dat.MON_PLAN_ID, 
+            dat.MON_LOC_ID, 
+            dat.RPT_PERIOD_ID,
+            dat.HOUR_ID,
+            camdecmps.format_date_hour( dat.BEGIN_DATE, dat.BEGIN_HOUR, null ) AS date_hour,
+            dat.OP_TIME,
+            dat.HR_LOAD,
+            dat.LOAD_UOM_CD,
+            dat.HI_FORMULA_CD,
+            dat.HI_RPT_VALUE,
+            dat.HI_CALC_VALUE,
+            dat.SO2_FORMULA_CD,
+            dat.SO2_RPT_VALUE,
+            dat.SO2_CALC_VALUE,
+            dat.NOXR_FORMULA_CD,
+            dat.NOXR_RPT_VALUE,
+            dat.NOXR_CALC_VALUE,
+            dat.NOX_FORMULA_CD,
+            dat.NOX_RPT_VALUE,
+            dat.NOX_CALC_VALUE,
+            dat.CO2_FORMULA_CD,
+            dat.CO2_RPT_VALUE,
+            dat.CO2_CALC_VALUE,
+            dat.FLOW_CALC_ADJ_VALUE,
+            dat.FLOW_RPT_ADJ_VALUE,
+            dat.FLOW_RPT_UNADJ_VALUE,
+            (
+                select  case when max( coalesce( sev.SEVERITY_LEVEL, 0 ) ) > 0 then 'Y' else NULL end
+                  from  camdecmpsaux.CHECK_LOG chl
+                        left join camdecmpsmd.SEVERITY_CODE sev
+                          on sev.SEVERITY_CD = chl.SEVERITY_CD
+                 where  chl.CHK_SESSION_ID = dat.CHK_SESSION_ID
+                   and  chl.MON_LOC_ID = dat.MON_LOC_ID
+                   and  ( chl.OP_BEGIN_DATE < dat.BEGIN_DATE or ( chl.OP_BEGIN_DATE = dat.BEGIN_DATE and chl.OP_BEGIN_HOUR <= dat.BEGIN_HOUR ) )
+                   and  ( chl.OP_END_DATE > dat.BEGIN_DATE or ( chl.OP_END_DATE = dat.BEGIN_DATE and chl.OP_END_HOUR >= dat.BEGIN_HOUR ) )
+            ) as error_codes
+      from  (
+                select  hod.HOUR_ID,
+                        mpl.MON_PLAN_ID, 
+                        hod.MON_LOC_ID, 
+                        hod.RPT_PERIOD_ID,
+                        hod.BEGIN_DATE,
+                        hod.BEGIN_HOUR,
+                        hod.OP_TIME,
+                        hod.HR_LOAD,
+                        hod.LOAD_UOM_CD,
+                        ems.CHK_SESSION_ID,
+                        -- HI
+                        max( case when dhv.PARAMETER_CD = 'HI'   then frm.EQUATION_CD end ) as HI_FORMULA_CD,
+                        max( case when dhv.PARAMETER_CD = 'HI'   then dhv.ADJUSTED_HRLY_VALUE end ) as HI_RPT_VALUE,
+                        max( case when dhv.PARAMETER_CD = 'HI'   then dhv.CALC_ADJUSTED_HRLY_VALUE end ) as HI_CALC_VALUE,
+                        -- SO2 MASS
+                        max( case when dhv.PARAMETER_CD = 'SO2'  then frm.EQUATION_CD end ) as SO2_FORMULA_CD,
+                        max( case when dhv.PARAMETER_CD = 'SO2'  then dhv.ADJUSTED_HRLY_VALUE end ) as SO2_RPT_VALUE,
+                        max( case when dhv.PARAMETER_CD = 'SO2'  then dhv.CALC_ADJUSTED_HRLY_VALUE end ) as SO2_CALC_VALUE,
+                        -- NOX RATE
+                        max( case when dhv.PARAMETER_CD = 'NOXR' then frm.EQUATION_CD end ) as NOXR_FORMULA_CD,
+                        max( case when dhv.PARAMETER_CD = 'NOXR' then dhv.ADJUSTED_HRLY_VALUE end ) as NOXR_RPT_VALUE,
+                        max( case when dhv.PARAMETER_CD = 'NOXR' then dhv.CALC_ADJUSTED_HRLY_VALUE end ) as NOXR_CALC_VALUE,
+                        -- NOX MASS
+                        max( case when dhv.PARAMETER_CD = 'NOX' then frm.EQUATION_CD end ) as NOX_FORMULA_CD,
+                        max( case when dhv.PARAMETER_CD = 'NOX' then dhv.ADJUSTED_HRLY_VALUE end ) as NOX_RPT_VALUE,
+                        max( case when dhv.PARAMETER_CD = 'NOX' then dhv.CALC_ADJUSTED_HRLY_VALUE end ) as NOX_CALC_VALUE,
+                        -- CO2 MASS
+                        max( case when dhv.PARAMETER_CD = 'CO2' then frm.EQUATION_CD end ) as CO2_FORMULA_CD,
+                        max( case when dhv.PARAMETER_CD = 'CO2' then dhv.ADJUSTED_HRLY_VALUE end ) as CO2_RPT_VALUE,
+                        max( case when dhv.PARAMETER_CD = 'CO2' then dhv.CALC_ADJUSTED_HRLY_VALUE end ) as CO2_CALC_VALUE,
+                        -- FLOW
+                        flow.CALC_ADJUSTED_HRLY_VALUE AS FLOW_CALC_ADJ_VALUE,
+                        flow.ADJUSTED_HRLY_VALUE AS FLOW_RPT_ADJ_VALUE, 
+                        flow.UNADJUSTED_HRLY_VALUE AS FLOW_RPT_UNADJ_VALUE
+                  from  (
+                            select  'MDC-4F0130B46ADB4B4F9C2EE0BA4A72D463' as MON_PLAN_ID,
+                                    123 as RPT_PERIOD_ID
+                        ) sel
+                        join camdecmps.MONITOR_PLAN_LOCATION mpl
+                          on mpl.MON_PLAN_ID = sel.MON_PLAN_ID
+                        join camdecmps.EMISSION_EVALUATION ems
+                          on ems.RPT_PERIOD_ID = sel.RPT_PERIOD_ID
+                         and ems.MON_PLAN_ID = mpl.MON_PLAN_ID
+                        join camdecmps.HRLY_OP_DATA hod 
+                          on hod.rpt_period_id = ems.rpt_period_id
+                         and hod.mon_loc_id = mpl.mon_loc_id
+                        left join camdecmps.MONITOR_HRLY_VALUE flow
+                          on flow.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
+                         and flow.MON_LOC_ID = hod.MON_LOC_ID
+                         and flow.HOUR_ID = hod.HOUR_ID
+                         and flow.PARAMETER_CD = 'FLOW'
+                        left join camdecmps.DERIVED_HRLY_VALUE dhv
+                          on dhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
+                         and dhv.MON_LOC_ID = hod.MON_LOC_ID
+                         and dhv.HOUR_ID = hod.HOUR_ID
+                         and dhv.PARAMETER_CD in ( 'CO2', 'HI', 'NOX', 'NOXR', 'SO2' )
+                        left join camdecmps.MONITOR_FORMULA frm
+                          on frm.MON_FORM_ID = dhv.MON_FORM_ID
+                 where  (
+                            flow.HOUR_ID is not null or
+                            dhv.HOUR_ID is not null
+                        )
+                 group
+                    by  hod.hour_id,
+                        mpl.MON_PLAN_ID, 
+                        hod.MON_LOC_ID, 
+                        hod.RPT_PERIOD_ID,
+                        hod.BEGIN_DATE,
+                        hod.BEGIN_HOUR,
+                        hod.OP_TIME,
+                        hod.HR_LOAD,
+                        hod.LOAD_UOM_CD,
+                        flow.CALC_ADJUSTED_HRLY_VALUE,
+                        flow.ADJUSTED_HRLY_VALUE, 
+                        flow.UNADJUSTED_HRLY_VALUE,
+                        ems.CHK_SESSION_ID
+        ) dat;
 
   RAISE NOTICE 'Refreshing view counts...';
   CALL camdecmps.refresh_emission_view_count(vmonplanid, vrptperiodid, 'ALL');
