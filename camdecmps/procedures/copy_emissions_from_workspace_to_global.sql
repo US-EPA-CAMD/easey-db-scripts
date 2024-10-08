019cab54b01d463cac2c0553e6551ac8333970b9
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-10-08 11:37:03 -0400

    Fixing defects on tickets 6080

--- camdecmps/procedures/copy_emissions_from_workspace_to_global.sql
+++ camdecmps/procedures/copy_emissions_from_workspace_to_global.sql
@@ -1389,5 +1389,16 @@ SELECT
   -- EMISSIONS VIEWS
   call camdecmps.copy_emissions_views_from_workspace_to_global(monplanid, rptperiodid);
 
+  -- DELETE WORKSPACE DATA
+  DELETE
+  FROM   camdecmpswks.check_session
+  WHERE  chk_session_id =
+         (
+                SELECT chk_session_id
+                FROM   camdecmpswks.emission_evaluation
+                WHERE  mon_plan_id = monplanid
+                AND    rpt_period_id = rptperiodid );
+
+  CALL camdecmpswks.delete_monitor_plan_emissions_data_from_workspace(monPlanId, rptPeriodId);
 END
 $BODY$;
