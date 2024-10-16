a93fb953d2102ea470aabab2b469a93004d8d428
Mark Hayward <mark.hayward@erg.com>
2024-10-16 16:18:04 -0400

    #6233 add required columns to insert statement

--- deployments/ecmps/release-v2.0-fix/Sprint10/6233/camdecmpswks/unit.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint10/6233/camdecmpswks/unit.sql
@@ -1,8 +1,10 @@
 --This is to copy initial data from camd.unit to camdecmpswks.unit
 
-INSERT INTO camdecmpswks.unit (unit_id, non_load_based_ind, userid, add_date, update_date)
+INSERT INTO camdecmpswks.unit (fac_id, unit_id, unitid, non_load_based_ind, userid, add_date, update_date)
 SELECT
+    fac_id, 
     unit_id,
+    unitid,
     non_load_based_ind,
     userid,
     add_date,
