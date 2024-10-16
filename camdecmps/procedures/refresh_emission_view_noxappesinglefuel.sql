dd993ed3fd18f77b1a12d59649bc7dc450d5e4e7
Dwayne Whitten <Dwayne.Whitten@erg.com>
2024-10-16 17:22:22 -0400

    #6410 Emission View SP NOXAPPESINGLEFUEL Changes

--- camdecmps/procedures/refresh_emission_view_noxappesinglefuel.sql
+++ camdecmps/procedures/refresh_emission_view_noxappesinglefuel.sql
@@ -1,18 +1,25 @@
-CREATE OR REPLACE PROCEDURE camdecmps.refresh_emission_view_noxappesinglefuel(IN vmonplanid character varying, IN vrptperiodid numeric)
+CREATE OR REPLACE PROCEDURE camdecmps.refresh_emission_view_noxappesinglefuel
+(
+    IN vmonplanid character varying,
+    IN vrptperiodid NUMERIC
+)
  LANGUAGE plpgsql
 AS $procedure$
 declare 
-	dhvParamCodes text[] := ARRAY['NOXR', 'HI', 'NOX'];
-	hpffParamCodes text[] := ARRAY['NOXR', 'HI'];
-BEGIN
-  RAISE NOTICE 'Loading temp_hourly_test_errors...';
-	CALL camdecmps.load_temp_hourly_test_errors(vMonPlanId, vRptPeriodId);
-
-  RAISE NOTICE 'Deleting existing records...';
-	DELETE FROM camdecmps.EMISSION_VIEW_NOXAPPESINGLEFUEL
-	WHERE MON_PLAN_ID = vMonPlanId AND RPT_PERIOD_ID = vRptPeriodId;
-
-  RAISE NOTICE 'Refreshing view data...';
+    monlocids text[];
+BEGIN    
+    select  array
+            (
+                select  MON_LOC_ID
+                  from  camdecmps.MONITOR_PLAN_LOCATION
+                 where  MON_PLAN_ID = vmonplanid
+            )
+      into  monLocIds;
+    
+
+    RAISE NOTICE 'Deleting existing records...';
+	DELETE FROM camdecmps.EMISSION_VIEW_NOXAPPESINGLEFUEL	WHERE MON_PLAN_ID = vMonPlanId AND RPT_PERIOD_ID = vRptPeriodId;
+    RAISE NOTICE 'Refreshing view data...';
 	INSERT INTO camdecmps.EMISSION_VIEW_NOXAPPESINGLEFUEL(
 		MON_PLAN_ID,
 		MON_LOC_ID,
@@ -39,130 +46,193 @@ BEGIN
 		CALC_NOX_MASS_ALL_FUELS,
 		ERROR_CODES
 	)
-	SELECT
-		HOD.MON_PLAN_ID, 
-		HOD.MON_LOC_ID, 
-		HOD.RPT_PERIOD_ID,
-		camdecmps.format_date_hour(hod.BEGIN_DATE, hod.BEGIN_HOUR, null),
-		HOD.OP_TIME,
-		COALESCE(MS.SYSTEM_IDENTIFIER, '') AS FUEL_SYS_ID,
-		HFF.FUEL_CD AS FUEL_TYPE,
-		HFF.FUEL_USAGE_TIME AS FUEL_USE_TIME,
-		HOD.HR_LOAD AS UNIT_LOAD, 
-		HOD.LOAD_UOM_CD AS LOAD_UOM, 
-		HPFF.HI_CALC_PARAM_VAL_FUEL AS CALC_HI_RATE,
-		HPFF.NOXR_OPERATING_CONDITION_CD,
-		HPFF.NOXR_SEGMENT_NUM AS SEGMENT_NUMBER,
-		MS_NOXR.SYSTEM_IDENTIFIER AS APP_E_SYS_ID,
-		HPFF.NOXR_PARAM_VAL_FUEL AS RPT_NOX_EMISSION_RATE,
-		HPFF.NOXR_CALC_PARAM_VAL_FUEL AS CALC_NOX_EMISSION_RATE,
-		NOXR_MF.EQUATION_CD AS SUMMATION_FORMULA_CD,
-		DHV.NOXR_ADJUSTED_HRLY_VALUE AS RPT_NOX_EMISSION_RATE_ALL_FUELS,
-		DHV.NOXR_CALC_ADJUSTED_HRLY_VALUE AS CALC_NOX_EMISSION_RATE_ALL_FUELS,
-		CASE 
-			WHEN (DHV.NOXR_HOUR_ID IS NOT NULL) THEN DHV.HI_CALC_ADJUSTED_HRLY_VALUE
-		END AS CALC_HI_RATE_ALL_FUELS,
-		NOX_MF.EQUATION_CD AS NOX_MASS_RATE_FORMULA_CD,
-		DHV.NOX_ADJUSTED_HRLY_VALUE AS RPT_NOX_MASS_ALL_FUELS,
-		DHV.NOX_CALC_ADJUSTED_HRLY_VALUE AS CALC_NOX_MASS_ALL_FUELS,
-		HOD.ERROR_CODES
-	FROM temp_hourly_test_errors AS HOD 
-	JOIN camdecmps.HRLY_FUEL_FLOW HFF 
-		ON HFF.HOUR_ID = HOD.HOUR_ID
-		AND HFF.MON_LOC_ID = HOD.MON_LOC_ID
-		AND HFF.RPT_PERIOD_ID = HOD.RPT_PERIOD_ID	
-	LEFT JOIN camdecmps.get_hourly_param_fuel_flow_pivoted(
-	  vmonplanid, vrptperiodid, hpffParamCodes
-	) AS hpff (
-		hrly_fuel_flow_id character varying,
-		hour_id character varying,
-		mon_loc_id character varying,
-		rpt_period_id numeric,
-		noxr_hrly_fuel_flow_id character varying,
-		noxr_mon_sys_id character varying,
-		noxr_mon_form_id character varying,
-		noxr_param_val_fuel numeric,
-		noxr_calc_param_val_fuel numeric,
-		noxr_segment_num numeric,
-		noxr_sample_type_cd character varying,
-		noxr_parameter_uom_cd character varying,
-		noxr_operating_condition_cd character varying,
-		hi_hrly_fuel_flow_id character varying,
-		hi_mon_sys_id character varying,
-		hi_mon_form_id character varying,
-		hi_param_val_fuel numeric,
-		hi_calc_param_val_fuel numeric,
-		hi_segment_num numeric,
-		hi_sample_type_cd character varying,
-		hi_parameter_uom_cd character varying,
-		hi_operating_condition_cd character varying
-	)   ON hpff.HRLY_FUEL_FLOW_ID = hff.HRLY_FUEL_FLOW_ID
-		AND hpff.HOUR_ID = hff.HOUR_ID
-		AND hpff.MON_LOC_ID = hff.MON_LOC_ID
-		AND hpff.RPT_PERIOD_ID = hff.RPT_PERIOD_ID
-	LEFT JOIN camdecmps.get_derived_hourly_values_pivoted(
-			vmonplanid, vrptperiodid, dhvParamCodes
-		) AS dhv (
-			hour_id character varying,
-			mon_loc_id character varying,
-			rpt_period_id numeric,
-			noxr_hour_id character varying,
-			noxr_mon_sys_id character varying,
-			noxr_mon_form_id character varying,
-			noxr_adjusted_hrly_value numeric,
-			noxr_calc_adjusted_hrly_value numeric,
-			noxr_unadjusted_hrly_value numeric,
-			noxr_calc_unadjusted_hrly_value numeric,
-			noxr_applicable_bias_adj_factor numeric,
-			noxr_calc_pct_moisture numeric,
-			noxr_calc_pct_diluent numeric,
-			noxr_pct_available numeric,
-			noxr_segment_num numeric,
-			noxr_operating_condition_cd character varying,
-			noxr_fuel_cd character varying,
-			noxr_modc_cd character varying,
-			hi_hour_id character varying,
-			hi_mon_sys_id character varying,
-			hi_mon_form_id character varying,
-			hi_adjusted_hrly_value numeric,
-			hi_calc_adjusted_hrly_value numeric,
-			hi_unadjusted_hrly_value numeric,
-			hi_calc_unadjusted_hrly_value numeric,
-			hi_applicable_bias_adj_factor numeric,
-			hi_calc_pct_moisture numeric,
-			hi_calc_pct_diluent numeric,
-			hi_pct_available numeric,
-			hi_segment_num numeric,
-			hi_operating_condition_cd character varying,
-			hi_fuel_cd character varying,
-			hi_modc_cd character varying,
-			nox_hour_id character varying,
-			nox_mon_sys_id character varying,
-			nox_mon_form_id character varying,
-			nox_adjusted_hrly_value numeric,
-			nox_calc_adjusted_hrly_value numeric,
-			nox_unadjusted_hrly_value numeric,
-			nox_calc_unadjusted_hrly_value numeric,
-			nox_applicable_bias_adj_factor numeric,
-			nox_calc_pct_moisture numeric,
-			nox_calc_pct_diluent numeric,
-			nox_pct_available numeric,
-			nox_segment_num numeric,
-			nox_operating_condition_cd character varying,
-			nox_fuel_cd character varying,
-			nox_modc_cd character varying
-	) ON dhv.HOUR_ID = hod.HOUR_ID
-		AND dhv.MON_LOC_ID = hod.MON_LOC_ID
-		AND dhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
-	LEFT JOIN camdecmps.MONITOR_SYSTEM MS 
-		ON HFF.MON_SYS_ID = MS.MON_SYS_ID
-	LEFT JOIN camdecmps.MONITOR_SYSTEM MS_NOXR 
-		ON HPFF.NOXR_MON_SYS_ID = MS_NOXR.MON_SYS_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA NOXR_MF 
-		ON DHV.NOXR_MON_FORM_ID = NOXR_MF.MON_FORM_ID
-	LEFT JOIN camdecmps.MONITOR_FORMULA NOX_MF 
-		ON DHV.NOX_MON_FORM_ID = NOX_MF.MON_FORM_ID
-	WHERE hpff.noxr_hrly_fuel_flow_id is not null;
+    select  hod.MON_PLAN_ID, 
+            hod.MON_LOC_ID, 
+            hod.RPT_PERIOD_ID, 
+            camdecmps.format_date_hour( hod.BEGIN_DATE, hod.BEGIN_HOUR, null ) as DATE_HOUR,
+            hod.OP_TIME, 
+            coalesce( ms.SYSTEM_IDENTIFIER, '' ) as FUEL_SYS_ID,
+            hff.FUEL_CD as FUEL_TYPE,
+            hff.FUEL_USAGE_TIME as FUEL_USE_TIME,
+            hod.HR_LOAD as UNIT_LOAD, 
+            hod.LOAD_UOM_CD as LOAD_UOM,
+            hpff.HI_CALC_PARAM_VAL_FUEL as CALC_HI_RATE,
+            hpff.NOXR_OPERATING_CONDITION_CD,
+            hpff.NOXR_SEGMENT_NUM as SEGMENT_NUMBER,
+            ms_noxr.SYSTEM_IDENTIFIER as APP_E_SYS_ID,
+            hpff.NOXR_PARAM_VAL_FUEL as RPT_NOX_EMISSION_RATE,
+            hpff.NOXR_CALC_PARAM_VAL_FUEL as CALC_NOX_EMISSION_RATE,
+            noxr_mf.EQUATION_CD as SUMMATION_FORMULA_CD,
+            dhv.NOXR_ADJUSTED_HRLY_VALUE as RPT_NOX_EMISSION_RATE_ALL_FUELS,
+            dhv.NOXR_CALC_ADJUSTED_HRLY_VALUE as CALC_NOX_EMISSION_RATE_ALL_FUELS,
+            case 
+                when ( dhv.NOXR_HOUR_ID is not null )
+                then dhv.HI_CALC_ADJUSTED_HRLY_VALUE
+            end as CALC_HI_RATE_ALL_FUELS,
+            nox_mf.EQUATION_CD as NOX_MASS_RATE_FORMULA_CD,
+            dhv.NOX_ADJUSTED_HRLY_VALUE as RPT_NOX_MASS_ALL_FUELS,
+            dhv.NOX_CALC_ADJUSTED_HRLY_VALUE as CALC_NOX_MASS_ALL_FUELS,
+            hod.ERROR_CODES
+      from  (
+                select  sel.MON_PLAN_ID, 
+                        hod.MON_LOC_ID, 
+                        sel.RPT_PERIOD_ID,
+                        hod.HOUR_ID,
+                        hod.BEGIN_DATE,
+                        hod.BEGIN_HOUR,
+                        hod.OP_TIME,
+                        hod.HR_LOAD,
+                        hod.LOAD_UOM_CD,
+                        ' ' as ERROR_CODES
+                  from  (
+                            select  vmonplanid as MON_PLAN_ID,
+                                    vrptperiodid as RPT_PERIOD_ID
+                        ) sel
+                        join camdecmps.MONITOR_PLAN_LOCATION mpl
+                          on mpl.MON_PLAN_ID = sel.MON_PLAN_ID
+                        join camdecmps.EMISSION_EVALUATION ems
+                          on ems.RPT_PERIOD_ID = sel.RPT_PERIOD_ID
+                         and ems.MON_PLAN_ID = mpl.MON_PLAN_ID
+                        join camdecmps.HRLY_OP_DATA hod 
+                          on hod.rpt_period_id = ems.rpt_period_id
+                         and hod.mon_loc_id = mpl.mon_loc_id
+            ) as hod
+            join camdecmps.HRLY_FUEL_FLOW hff
+              on hff.HOUR_ID = hod.HOUR_ID
+             and hff.MON_LOC_ID = hod.MON_LOC_ID
+             and hff.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
+            left join
+            (
+                select  hff.hrly_fuel_flow_id,
+                        hff.hour_id,
+                        hff.mon_loc_id,
+                        hff.rpt_period_id,
+                        noxr.hrly_fuel_flow_id          as noxr_hrly_fuel_flow_id,           
+                        noxr.mon_sys_id                 as noxr_mon_sys_id,                  
+                        noxr.mon_form_id                as noxr_mon_form_id,                 
+                        noxr.param_val_fuel             as noxr_param_val_fuel,              
+                        noxr.calc_param_val_fuel        as noxr_calc_param_val_fuel,         
+                        noxr.segment_num                as noxr_segment_num,
+                        noxr.operating_condition_cd     as noxr_operating_condition_cd,
+                        hi.calc_param_val_fuel          as hi_calc_param_val_fuel
+                  from  camdecmps.HRLY_FUEL_FLOW hff
+                        left join
+                        (
+                            select  hrly_fuel_flow_id,
+                                    mon_loc_id,
+                                    rpt_period_id,
+                                    mon_sys_id,
+                                    mon_form_id,
+                                    param_val_fuel,
+                                    calc_param_val_fuel,
+                                    segment_num,
+                                    operating_condition_cd
+                              from  camdecmps.HRLY_PARAM_FUEL_FLOW
+                             where  mon_loc_id = any( monLocIds )
+                               and  rpt_period_id = vrptperiodid
+                               and  parameter_cd = 'NOXR'
+                        ) noxr
+                          on noxr.hrly_fuel_flow_id = hff.hrly_fuel_flow_id
+                         and noxr.mon_loc_id = hff.mon_loc_id
+                         and noxr.rpt_period_id = hff.rpt_period_id
+                        left join
+                        (
+                            select  hrly_fuel_flow_id,
+                                    mon_loc_id,
+                                    rpt_period_id,
+                                    calc_param_val_fuel
+                              from  camdecmps.HRLY_PARAM_FUEL_FLOW
+                             where  mon_loc_id = any( monLocIds )
+                               and  rpt_period_id = vrptperiodid
+                               and  parameter_cd = 'HI'
+                        ) hi
+                          on hi.hrly_fuel_flow_id = hff.hrly_fuel_flow_id
+                         and hi.mon_loc_id = hff.mon_loc_id
+                         and hi.rpt_period_id = hff.rpt_period_id
+                 where  hff.MON_LOC_ID = any( monLocIds )
+                   and  hff.RPT_PERIOD_ID = vrptperiodid
+            ) hpff
+              on hpff.HRLY_FUEL_FLOW_ID = hff.HRLY_FUEL_FLOW_ID
+             and hpff.HOUR_ID = hff.HOUR_ID
+             and hpff.MON_LOC_ID = hff.MON_LOC_ID
+             and hpff.RPT_PERIOD_ID = hff.RPT_PERIOD_ID
+            left join
+            (
+                select  hod.hour_id,
+                        hod.mon_loc_id,
+                        hod.rpt_period_id,
+                        noxr.hour_id                    as noxr_hour_id,   
+                        noxr.mon_sys_id                 as noxr_mon_sys_id,
+                        noxr.mon_form_id                as noxr_mon_form_id,
+                        noxr.adjusted_hrly_value        as noxr_adjusted_hrly_value,
+                        noxr.calc_adjusted_hrly_value   as noxr_calc_adjusted_hrly_value,
+                        hi.calc_adjusted_hrly_value     as hi_calc_adjusted_hrly_value,
+                        nox.mon_form_id                 as nox_mon_form_id,
+                        nox.adjusted_hrly_value         as nox_adjusted_hrly_value,
+                        nox.calc_adjusted_hrly_value    as nox_calc_adjusted_hrly_value
+                  from  camdecmps.HRLY_OP_DATA hod
+                        left join
+                        (
+                            select  hour_id,
+                                    mon_loc_id,
+                                    rpt_period_id,
+                                    mon_sys_id,
+                                    mon_form_id,
+                                    adjusted_hrly_value,
+                                    calc_adjusted_hrly_value
+                              from  camdecmps.DERIVED_HRLY_VALUE
+                             where  mon_loc_id = any( monLocIds )
+                               and  rpt_period_id = vrptperiodid
+                               and  parameter_cd = 'NOXR'
+                        ) noxr
+                          on noxr.hour_id = hod.hour_id
+                         and noxr.mon_loc_id = hod.mon_loc_id
+                         and noxr.rpt_period_id = hod.rpt_period_id
+                        left join
+                        (
+                            select  hour_id,
+                                    mon_loc_id,
+                                    rpt_period_id,
+                                    calc_adjusted_hrly_value
+                              from  camdecmps.DERIVED_HRLY_VALUE
+                             where  mon_loc_id = any( monLocIds )
+                               and  rpt_period_id = vrptperiodid
+                               and  parameter_cd = 'HI'
+                        ) hi
+                          on hi.hour_id = hod.hour_id
+                         and hi.mon_loc_id = hod.mon_loc_id
+                         and hi.rpt_period_id = hod.rpt_period_id
+                        left join
+                        (
+                            select  hour_id,
+                                    mon_loc_id,
+                                    rpt_period_id,
+                                    mon_form_id,
+                                    adjusted_hrly_value,
+                                    calc_adjusted_hrly_value
+                              from  camdecmps.DERIVED_HRLY_VALUE
+                             where  mon_loc_id = any( monLocIds )
+                               and  rpt_period_id = vrptperiodid
+                               and  parameter_cd = 'NOX'
+                        ) nox
+                          on nox.hour_id = hod.hour_id
+                         and nox.mon_loc_id = hod.mon_loc_id
+                         and nox.rpt_period_id = hod.rpt_period_id
+                 where  hod.MON_LOC_ID = any( monLocIds )
+                   and  hod.RPT_PERIOD_ID = vrptperiodid
+            ) dhv
+              ON dhv.HOUR_ID = hod.HOUR_ID
+             AND dhv.MON_LOC_ID = hod.MON_LOC_ID
+             AND dhv.RPT_PERIOD_ID = hod.RPT_PERIOD_ID
+            left join camdecmps.MONITOR_SYSTEM ms
+                on ms.MON_SYS_ID = hff.MON_SYS_ID
+            left join camdecmps.MONITOR_SYSTEM ms_noxr
+                on ms_noxr.MON_SYS_ID = hpff.NOXR_MON_SYS_ID
+            left join camdecmps.MONITOR_FORMULA noxr_mf
+                on noxr_mf.MON_FORM_ID = dhv.NOXR_MON_FORM_ID
+            left join camdecmps.MONITOR_FORMULA nox_mf
+                on nox_mf.MON_FORM_ID = dhv.NOX_MON_FORM_ID
+     WHERE  hpff.noxr_hrly_fuel_flow_id is not null;
 
 	
 
