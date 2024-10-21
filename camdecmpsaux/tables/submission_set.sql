4fe4160336df4e80943986a7ae5062f2984c15fa
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-10-21 09:17:11 -0400

    Add Additional Date Fields to Submission Queue Table

--- camdecmpsaux/tables/submission_set.sql
+++ camdecmpsaux/tables/submission_set.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS camdecmpsaux.submission_set
 (
     submission_set_id character varying(45) COLLATE pg_catalog."default" NOT NULL,
     mon_plan_id character varying(45) COLLATE pg_catalog."default" NOT NULL,
-    submitted_on timestamp without time zone NOT NULL,
+    //submitted_on timestamp without time zone NOT NULL,
+    queued_time timestamp without time zone NOT NULL,
     user_id character varying(160) COLLATE pg_catalog."default" NOT NULL,
     user_email character varying(100) COLLATE pg_catalog."default" NOT NULL,
     fac_id numeric(38,0) NOT NULL,
@@ -11,7 +12,11 @@ CREATE TABLE IF NOT EXISTS camdecmpsaux.submission_set
     activity_id text COLLATE pg_catalog."default" NOT NULL,
     configuration text COLLATE pg_catalog."default" NOT NULL,
     status_cd character varying(8) COLLATE pg_catalog."default",
-    details text COLLATE pg_catalog."default",
-    submission_end_stage_time timestamp without time zone,
+    //details text COLLATE pg_catalog."default",
+    //submission_end_stage_time timestamp without time zone,
+    started_time timestamp without time zone,
+    completed_time timestamp without time zone,
+    note text COLLATE pg_catalog."default",
+    note_time timestamp without time zone
 	has_crit_errors bool
 )
