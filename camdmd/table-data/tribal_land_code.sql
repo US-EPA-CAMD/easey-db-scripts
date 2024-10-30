5a82dc0f98a030e0bcab4bab46f32cff18fc9bf6
Anna <anna.yang@erg.com>
2024-10-30 14:43:22 -0400

    Add files via upload

---
+++ camdmd/table-data/tribal_land_code.sql
@@ -0,0 +1,2 @@
+INSERT INTO camdmd.tribal_land_code (tribal_land_cd, tribal_land_description) VALUES ('NAVAJO', 'Navajo Nation');
+INSERT INTO camdmd.tribal_land_code (tribal_land_cd, tribal_land_description) VALUES ('UTE', 'Ute Indian Tribe');
