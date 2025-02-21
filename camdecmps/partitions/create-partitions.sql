c6f23badf9a7beb692572150d0f4154a96772628
DWhitten <Dwayne.Whitten@erg.com>
2025-02-21 14:52:50 -0500

    #6569 Correcting begin year in the create-partitions.sql script, and adding the create-partitions procedure script to the Deployment folder.

--- camdecmps/partitions/create-partitions.sql
+++ camdecmps/partitions/create-partitions.sql
@@ -2,7 +2,7 @@ DO $$
 DECLARE vYear smallint;
 BEGIN 
   SELECT EXTRACT('year' FROM CURRENT_DATE) into vYear;  
-  for counter in 2024..vYear loop
+  for counter in 2003..vYear loop
        call camdecmps.create-partitions(counter, 'true');
    end loop;
 END $$
