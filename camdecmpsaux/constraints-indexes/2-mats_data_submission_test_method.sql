24521943656bab33547849fcaf5d1d0663ca76b4
Jon Maxwell Diebold <max.diebold@erg.com>
2025-03-06 11:45:08 -0500

    fix(6525): added missing indexes

--- camdecmpsaux/constraints-indexes/2-mats_data_submission_test_method.sql
+++ camdecmpsaux/constraints-indexes/2-mats_data_submission_test_method.sql
@@ -3,3 +3,5 @@ ALTER TABLE IF EXISTS camdecmpsaux.mats_data_submission_test_method
     ADD CONSTRAINT fk_mats_data_submission_test_method_mats_data_submission FOREIGN KEY (mats_data_sub_id) REFERENCES camdecmpsaux.mats_data_submission (mats_data_sub_id) MATCH simple,
     ADD CONSTRAINT fk_mats_data_submission_test_method_mats_test_method_code FOREIGN KEY (mats_test_meth_cd) REFERENCES camdecmpsmd.mats_test_method_code (mats_test_meth_cd) MATCH simple;
 
+CREATE INDEX IF NOT EXISTS idx_mats_data_submission_test_method_mats_data_sub_id ON camdecmpsaux.mats_data_submission_test_method USING btree (mats_data_sub_id ASC nulls LAST);
+
--- deployments/ecmps/release-v2.0-fix/Sprint18/6525/camdecmpsaux/constraints-indexes/2-mats_data_submission_pollutant.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint18/6525/camdecmpsaux/constraints-indexes/2-mats_data_submission_pollutant.sql
@@ -3,5 +3,5 @@ ALTER TABLE camdecmpsaux.mats_data_submission_pollutant
     ADD CONSTRAINT fk_mats_data_submission_pollutant_mats_data_submission FOREIGN KEY (mats_data_sub_id) REFERENCES camdecmpsaux.mats_data_submission (mats_data_sub_id) MATCH simple,
     ADD CONSTRAINT fk_mats_data_submission_pollutant_mats_pollutant_code FOREIGN KEY (mats_pollutant_cd) REFERENCES camdecmpsmd.mats_pollutant_code (mats_pollutant_cd) MATCH simple;
 
-CREATE INDEX IF NOT EXISTS idx_mats_data_submission_pollutant_mats_data_sub_id ON camdecmpsaux.mats_data_submission_pollutant USING btree (mats_data_sub_id);
+CREATE INDEX IF NOT EXISTS idx_mats_data_submission_pollutant_mats_data_sub_id ON camdecmpsaux.mats_data_submission_pollutant USING btree (mats_data_sub_id ASC nulls LAST);
 
