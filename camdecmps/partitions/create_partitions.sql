c80a7101d9b64abdd2aa91c5a0d19f3787404d96
Anna <anna.yang@erg.com>
2025-03-11 11:12:39 -0400

    correct file name

---
+++ camdecmps/partitions/create_partitions.sql
@@ -0,0 +1,8 @@
+DO $$
+DECLARE vYear smallint;
+BEGIN 
+  SELECT EXTRACT('year' FROM CURRENT_DATE) into vYear;  
+  for counter in 2003..vYear loop
+       call camdecmps.create-partitions(counter, 'true');
+   end loop;
+END $$
