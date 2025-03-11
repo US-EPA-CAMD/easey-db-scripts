ad1218a5ba93134130931d5a25c57048973beda2
Anna <anna.yang@erg.com>
2025-03-11 11:13:28 -0400

    Delete camdecmps/partitions/create-partitions.sql

--- camdecmps/partitions/create-partitions.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-DECLARE vYear smallint;
-BEGIN 
-  SELECT EXTRACT('year' FROM CURRENT_DATE) into vYear;  
-  for counter in 2003..vYear loop
-       call camdecmps.create-partitions(counter, 'true');
-   end loop;
-END $$
