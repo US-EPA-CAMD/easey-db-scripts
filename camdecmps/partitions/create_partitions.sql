5d12bd347f0a71b7b81a8da06ca30f43cfb5851e
Anna <anna.yang@erg.com>
2025-03-12 08:57:58 -0400

    Update create_partitions.sql

--- camdecmps/partitions/create_partitions.sql
+++ camdecmps/partitions/create_partitions.sql
@@ -3,6 +3,6 @@ DECLARE vYear smallint;
 BEGIN 
   SELECT EXTRACT('year' FROM CURRENT_DATE) into vYear;  
   for counter in 2003..vYear loop
-       call camdecmps.create-partitions(counter, 'true');
+       call camdecmps.create_partitions(counter, 'true');
    end loop;
 END $$
