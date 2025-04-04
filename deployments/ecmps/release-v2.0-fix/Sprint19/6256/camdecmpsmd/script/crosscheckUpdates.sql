583b9b642dc4fc47a1c233a91f4831c8a80d1ee2
Jonas Hernandez <jonas.hernandez@erg.com>
2025-04-04 11:42:47 -0500

    updated PR with the requested changes

--- deployments/ecmps/release-v2.0-fix/Sprint19/6256/camdecmpsmd/script/crosscheckUpdates.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint19/6256/camdecmpsmd/script/crosscheckUpdates.sql
@@ -21,3 +21,10 @@ SET value1 = 'PM'
 WHERE cross_chk_catalog_id = 16
   AND value1 = 'PMRE'
   AND value3 IN ('11-3', '11-16', '11-34', '11-37', '11-46');
+
+--PARAMETER_METHOD TO FORMULA
+--Change row where Parameter Code equals to "NOX" and Method Code equals "CALC"
+UPDATE camdecmpsmd.cross_check_catalog_value
+SET value2 = 'F-28'
+WHERE cross_chk_catalog_value_id = 8957
+  AND cross_chk_catalog_id = 8;
