2baa5f760b802f103900be6f35fca5637154dc37
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-04-28 14:42:47 -0400

    update camdecmpsmd.severity_code entries in add customizations.

--- camdecmpsmd/table-data/severity_code.sql
+++ camdecmpsmd/table-data/severity_code.sql
@@ -4,6 +4,6 @@ INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, sev
 INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('NONCRIT', 'Non-Critical Error', 3, 1, 'INFO');
 INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('FORGIVE', 'Forgiven', NULL, 0, 'INFO');
 INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('CRIT1', 'Critical Error Level 1', 6, 1, 'ERR');
-INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('CRIT2', 'Critical Error Level 2', 5, 1, 'ERR');
-INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('FATAL', 'Fatal', 7, 0, 'INFO');
+INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('CRIT2', 'Critical Error Level 2', 5, 1, 'INFO');
+INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('FATAL', 'Fatal', 7, 0, 'ERR');
 INSERT INTO camdecmpsmd.severity_code (severity_cd, severity_cd_description, severity_level, es_type_ind, eval_status_cd) VALUES ('CRIT3', 'Critical Error Level 3', 4, 0, 'INFO');
