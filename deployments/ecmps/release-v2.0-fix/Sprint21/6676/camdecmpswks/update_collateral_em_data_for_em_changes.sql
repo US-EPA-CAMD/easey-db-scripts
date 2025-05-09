f568167cb22262c9d6e1bf776312d2423fd4d302
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-05-09 15:49:29 -0400

    functionality to implement reset needs evaluation flag on collateral record updates.

--- deployments/ecmps/release-v2.0-fix/Sprint21/6676/camdecmpswks/update_collateral_em_data_for_em_changes.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint21/6676/camdecmpswks/update_collateral_em_data_for_em_changes.sql
@@ -12,51 +12,63 @@ CREATE OR REPLACE FUNCTION camdecmpswks.update_collateral_em_data_for_em_changes
     ROWS 1000
 
 AS $BODY$
-declare 
+declare
+    vSubmittable    char(1);
 	V_MON_PLAN_ID   character varying;
 	V_RPT_PERIOD_ID int;
 	EM_CSR 			CURSOR FOR SELECT MON_PLAN_ID, RPT_PERIOD_ID
 					FROM tmpEmissionsStatus;
 begin
+    vSubmittable :='N';
     error_msg := '';
     result := 'T';
 
-    create temp table tmpEmissionsStatus(MON_PLAN_ID character varying,RPT_PERIOD_ID int);
+    select  coalesce ( max( 'Y' ), 'N' ) as Submittable
+      into  vSubmittable
+      from  camdecmpsaux.EM_SUBMISSION_ACCESS
+     where  mon_plan_id = vmon_plan_id
+       and  sub_availability_cd IN ('GRANTED', 'REQUIRE')
+	   and  RPT_PERIOD_ID = vperiod_id;
 
-    INSERT INTO tmpEmissionsStatus
-     SELECT DISTINCT E.MON_PLAN_ID, E.RPT_PERIOD_ID
-       FROM camdecmpswks.EMISSION_EVALUATION E,
-            camdecmpsaux.EM_SUBMISSION_ACCESS ESA,
-            camdecmpswks.MONITOR_PLAN_LOCATION M,
-            camdecmpsmd.REPORTING_PERIOD R,
-            (SELECT MON_LOC_ID, EE.MON_PLAN_ID, EE.RPT_PERIOD_ID,CALENDAR_YEAR, QUARTER
-                FROM camdecmpswks.EMISSION_EVALUATION EE
-                    INNER JOIN camdecmpsmd.REPORTING_PERIOD RP ON EE.RPT_PERIOD_ID = RP.RPT_PERIOD_ID
-                    INNER JOIN camdecmpswks.MONITOR_PLAN_LOCATION MPL ON EE.MON_PLAN_ID = MPL.MON_PLAN_ID
-            ) T
-        WHERE E.MON_PLAN_ID = M.MON_PLAN_ID
-          AND E.RPT_PERIOD_ID = R.RPT_PERIOD_ID
-          AND E.MON_PLAN_ID = ESA.MON_PLAN_ID
-          AND E.RPT_PERIOD_ID = ESA.RPT_PERIOD_ID
-          AND ESA.SUB_AVAILABILITY_CD IN ('GRANTED','REQUIRE')
-          AND M.MON_LOC_ID = T.MON_LOC_ID
-          AND (R.CALENDAR_YEAR > T.CALENDAR_YEAR OR
-                (R.CALENDAR_YEAR = T.CALENDAR_YEAR AND R.QUARTER > T.QUARTER))
-          AND T.MON_PLAN_ID = vmon_plan_id
-          AND T.RPT_PERIOD_ID = vperiod_id;
+    if vSubmittable = 'Y' then
+        create temp table tmpEmissionsStatus(MON_PLAN_ID character varying,RPT_PERIOD_ID int);
 
-       OPEN EM_CSR;
-         LOOP
-            FETCH Next from EM_CSR INTO V_MON_PLAN_ID, V_RPT_PERIOD_ID;
-              Exit when not found;
-                  select * into result, error_msg
-                   from camdecmpswks.delete_calculated_em_data_from_workspace(V_MON_PLAN_ID, V_RPT_PERIOD_ID);
-                 -- Deleting Calculated data failed, bail (PJR)
-                 IF result = 'F' then
-                    exit;
-                  end if;
-             end loop;
-            CLOSE EM_CSR;
+        INSERT INTO tmpEmissionsStatus
+         SELECT DISTINCT E.MON_PLAN_ID, E.RPT_PERIOD_ID
+           FROM camdecmpswks.EMISSION_EVALUATION E,
+                camdecmpsaux.EM_SUBMISSION_ACCESS ESA,
+                camdecmpswks.MONITOR_PLAN_LOCATION M,
+                camdecmpsmd.REPORTING_PERIOD R,
+                (SELECT MON_LOC_ID, EE.MON_PLAN_ID, EE.RPT_PERIOD_ID,CALENDAR_YEAR, QUARTER
+                    FROM camdecmpswks.EMISSION_EVALUATION EE
+                        INNER JOIN camdecmpsmd.REPORTING_PERIOD RP ON EE.RPT_PERIOD_ID = RP.RPT_PERIOD_ID
+                        INNER JOIN camdecmpswks.MONITOR_PLAN_LOCATION MPL ON EE.MON_PLAN_ID = MPL.MON_PLAN_ID
+                ) T
+            WHERE E.MON_PLAN_ID = M.MON_PLAN_ID
+              AND E.RPT_PERIOD_ID = R.RPT_PERIOD_ID
+              AND E.MON_PLAN_ID = ESA.MON_PLAN_ID
+              AND E.RPT_PERIOD_ID = ESA.RPT_PERIOD_ID
+              AND ESA.SUB_AVAILABILITY_CD IN ('GRANTED','REQUIRE')
+              AND M.MON_LOC_ID = T.MON_LOC_ID
+              AND (R.CALENDAR_YEAR > T.CALENDAR_YEAR OR
+                    (R.CALENDAR_YEAR = T.CALENDAR_YEAR AND R.QUARTER > T.QUARTER))
+              AND T.MON_PLAN_ID = vmon_plan_id
+              AND T.RPT_PERIOD_ID = vperiod_id;
+
+           OPEN EM_CSR;
+             LOOP
+                FETCH Next from EM_CSR INTO V_MON_PLAN_ID, V_RPT_PERIOD_ID;
+                  Exit when not found;
+                      select * into result, error_msg
+                       from camdecmpswks.delete_calculated_em_data_from_workspace(V_MON_PLAN_ID, V_RPT_PERIOD_ID);
+                     -- Deleting Calculated data failed, bail (PJR)
+                     IF result = 'F' then
+                        exit;
+                      end if;
+                 end loop;
+                CLOSE EM_CSR;
+
+    end if; --vSubmittable if
 
     RETURN NEXT; -- Add row to return table.
   
