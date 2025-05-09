359a856446e33cda522f696eb1f5f4bfeb4d3685
Yonatan Amezene <yamezene@decisionpointcorp.com>
2025-05-09 16:15:56 -0400

    functionality to implement reset needs evaluation flag on collateral record updates.

--- camdecmpswks/functions/update_collateral_em_data_for_qat_changes.sql
+++ camdecmpswks/functions/update_collateral_em_data_for_qat_changes.sql
@@ -30,11 +30,12 @@ begin
     --Check no test_sum_id or can't submit
     SELECT count(*) into vCount
       FROM camdecmpswks.QA_SUPP_DATA
-     WHERE TEST_SUM_ID != vTestSumId
-        OR (TEST_SUM_ID = vTestSumId AND
-            (submission_availability_cd IS NULL OR
-            submission_availability_cd = 'GRANTED' OR
-            submission_availability_cd = 'REQUIRE' ));
+     WHERE TEST_SUM_ID = vTestSumId
+           AND (
+                submission_availability_cd IS NULL OR
+                submission_availability_cd = 'GRANTED' OR
+                submission_availability_cd = 'REQUIRE'
+                );
 
 	if vCount=0 then
 		vContinue:='N';
