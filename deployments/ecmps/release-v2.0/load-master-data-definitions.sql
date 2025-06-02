ad4878b4c0baccafdd363ca92c860982a80ef191
Anna <anna.yang@erg.com>
2025-06-02 16:36:04 -0400

    Add files via upload
    Add Missing MDM Endpoints #6753

--- deployments/ecmps/release-v2.0/load-master-data-definitions.sql
+++ deployments/ecmps/release-v2.0/load-master-data-definitions.sql
@@ -1166,4 +1166,51 @@ BEGIN
     VALUES
         (datatableId, 1, 'mats_avg_group_cd', 'matsAveragingGroupCode', 'MATS Averaging Group Code'),
         (datatableId, 2, 'mats_avg_group_description', 'matsAveragingGroupDescription', 'MATS Averaging Group Description');
+------------------------------------------------------------------------------------------------------------------------
+    datasetCode := 'reporting-frequency-codes';
+    INSERT INTO camdaux.dataset(dataset_cd, group_cd, display_name)
+    VALUES (datasetCode, groupCode, 'Report Frequency Codes & Descriptions');
+
+    /***** DATATABLE 1 *****/
+    INSERT INTO camdaux.datatable(dataset_cd, table_order, display_name, sql_statement)
+    VALUES (datasetCode, 1, 'Report Frequency Codes & Descriptions', 'SELECT * FROM camdecmpsmd.report_freq_code')
+    RETURNING datatable_id INTO datatableId;
+
+    /***** COLUMNS *****/
+    INSERT INTO camdaux.datacolumn(datatable_id, column_order, name, alias, display_name)
+    VALUES
+        (datatableId, 1, 'report_freq_cd', 'reportFrequencyCode', 'Report Frequency Code'),
+        (datatableId, 2, 'report_freq_cd_description', 'reportFrequencyCodeDescription', 'Report Frequency Code Descriptions');
+---------------------------------------------------------------------------------------------------------------------------
+    datasetCode := 'operating-type-codes';
+    INSERT INTO camdaux.dataset(dataset_cd, group_cd, display_name)
+    VALUES (datasetCode, groupCode, 'Operating Type Codes & Descriptions');
+
+    /***** DATATABLE 1 *****/
+    INSERT INTO camdaux.datatable(dataset_cd, table_order, display_name, sql_statement)
+    VALUES (datasetCode, 1, 'Operating Type Codes & Descriptions', 'SELECT * FROM camdecmpsmd.operating_type_code')
+    RETURNING datatable_id INTO datatableId;
+
+    /***** COLUMNS *****/
+    INSERT INTO camdaux.datacolumn(datatable_id, column_order, name, alias, display_name)
+    VALUES
+        (datatableId, 1, 'op_type_cd', 'operatingTypeCode', 'Operating Type Code'),
+        (datatableId, 2, 'op_type_cd_description', 'operatingTypeCodeDescription', 'Operating Type Code Description');
+--------------------------------------------------------------------------------------------------------------------------
+    datasetCode := 'file-type-codes';
+    INSERT INTO camdaux.dataset(dataset_cd, group_cd, display_name)
+    VALUES (datasetCode, groupCode, 'File Type Codes & Descriptions');
+
+    /***** DATATABLE 1 *****/
+    INSERT INTO camdaux.datatable(dataset_cd, table_order, display_name, sql_statement)
+    VALUES (datasetCode, 1, 'File Type Codes & Descriptions', 'SELECT * FROM camdecmpsmd.file_type_code')
+    RETURNING datatable_id INTO datatableId;
+
+    /***** COLUMNS *****/
+    INSERT INTO camdaux.datacolumn(datatable_id, column_order, name, alias, display_name)
+    VALUES
+        (datatableId, 1, 'file_type_cd', 'fileTypeCode', 'File Type Code'),
+        (datatableId, 2, 'file_type_cd_description', 'fileTypeCodeDescription', 'File Type Code Description');
+
+
 END $$;
