46304879257ba70a605ab96311aa15c6f57ddcc5
ergjustin <justin.joseph.erg.com>
2025-06-03 16:26:40 -0500

    updated script with cross_chk_catalog_id 195

--- deployments/ecmps/release-v2.0-fix/Sprint22/6702/add_rows_crosscheck_table.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint22/6702/add_rows_crosscheck_table.sql
@@ -7,3 +7,15 @@ INSERT INTO camdecmpsmd.cross_check_catalog_value (cross_chk_catalog_value_id, c
 -- Component Type Code to Basis Code (cross_chk_catalog_id 190):
 INSERT INTO camdecmpsmd.cross_check_catalog_value (cross_chk_catalog_value_id, cross_chk_catalog_id, value1, value2, value3) OVERRIDING SYSTEM VALUE VALUES (9335, 190, 'PM', 'D', null);
 INSERT INTO camdecmpsmd.cross_check_catalog_value (cross_chk_catalog_value_id, cross_chk_catalog_id, value1, value2, value3) OVERRIDING SYSTEM VALUE VALUES (9336, 190, 'PM', 'W', null);
+
+-- Parameter Code to Formula Code for Formulas (cross_chk_catalog_id 195):
+UPDATE camdecmpsmd.cross_check_catalog_value
+SET value1 = 'PM'
+WHERE cross_chk_catalog_id = 195
+  AND value1 = 'PMRE'
+  AND value2 IN ('11-16', '11-3', '11-34', '11-37', '11-46');
+
+DELETE FROM camdecmpsmd.cross_check_catalog_value
+WHERE cross_chk_catalog_id = 195
+  AND value1 = 'PMRH'
+  AND value2 IN ('11-16', '11-3', '11-34', '11-37', '11-46');
