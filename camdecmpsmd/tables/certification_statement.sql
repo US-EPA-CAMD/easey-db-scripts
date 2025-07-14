8314b3e349f545839e511d85388a131680b6024e
Jhernandez29 <jonashernandez@erg.com>
2025-07-14 16:29:57 -0500

    Added new camdecmpsmd.certification_statement with path location.

---
+++ camdecmpsmd/tables/certification_statement.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS camdecmpsmd.certification_statement
+(
+    statement_id integer NOT NULL,
+	prg_cd character varying(7) COLLATE pg_catalog."default" NULL,
+    statement_location character varying(200) COLLATE pg_catalog."default" NOT NULL,
+	display_order integer NOT NULL
+);
+
+COMMENT ON TABLE camdecmpsmd.certification_statement
+    IS 'Certification Statement Table.';
+
+COMMENT ON COLUMN camdecmpsmd.certification_statement.statement_id
+    IS 'Certification Statement Ids';
+
+COMMENT ON COLUMN camdecmpsmd.certification_statement.prg_cd
+    IS 'Program code values.';
+
+COMMENT ON COLUMN camdecmpsmd.certification_statement.statement_location
+    IS 'Path to certification statement files in the EASEY Content.';
+
+COMMENT ON COLUMN camdecmpsmd.certification_statement.display_order
+    IS 'Order using statement_id.';
+
+CREATE UNIQUE INDEX prg_cd_unique ON camdecmpsmd.certification_statement (prg_cd)
+WHERE prg_cd IS NOT NULL;
