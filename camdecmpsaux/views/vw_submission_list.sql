5008c0bef30799f814754cdc02f603a1af89edcc
Jhernandez29 <jonashernandez@erg.com>
2025-07-17 15:10:13 -0500

    added else to TEE

--- camdecmpsaux/views/vw_submission_list.sql
+++ camdecmpsaux/views/vw_submission_list.sql
@@ -73,7 +73,8 @@ select  fac.oris_code,
 	                	then tee.extens_exempt_cd || ' (' || prd.period_abbreviation || ') S: (' || sys.sys_type_cd || '/' || sys.system_identifier || ')'
                         when tee.extens_exempt_cd is not null and prd.period_abbreviation is not null and cmp.component_type_cd  is not null and cmp.component_identifier is not null
 	                	then tee.extens_exempt_cd || ' (' || prd.period_abbreviation || ') C: (' || cmp.component_type_cd || '/' || cmp.component_identifier ||  ')'
-                	end
+	                	else tee.extens_exempt_cd || ' (' || prd.period_abbreviation || ')'
+                    end
                       from  camdecmps.TEST_EXTENSION_EXEMPTION tee
                             join camdecmpsmd.REPORTING_PERIOD prd using ( rpt_period_id )
                             left join camdecmps.MONITOR_SYSTEM sys using ( mon_sys_id )
