7e1b47ab54a149fd33f91992ef81bf5d490a7cb3
Jhernandez29 <jonashernandez@erg.com>
2025-07-18 16:34:39 -0500

    ADD CONSTRAINT pk_certification_statement PRIMARY KEY (statement_id);

--- deployments/ecmps/release-v2.0-fix/Sprint25/camdecmpsmd/table/certification_statement.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint25/camdecmpsmd/table/certification_statement.sql
@@ -23,3 +23,6 @@ COMMENT ON COLUMN camdecmpsmd.certification_statement.display_order
 
 CREATE UNIQUE INDEX prg_cd_unique ON camdecmpsmd.certification_statement (prg_cd)
 WHERE prg_cd IS NOT NULL;
+
+ALTER TABLE IF EXISTS camdecmpsmd.certification_statement
+    ADD CONSTRAINT pk_certification_statement PRIMARY KEY (statement_id);
