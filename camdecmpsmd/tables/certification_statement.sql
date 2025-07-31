4777cf0eb312da847bb4e280ff860eac2bcd5bf8
Jhernandez29 <jonashernandez@erg.com>
2025-07-31 17:47:49 -0500

    Constraints and Indexes should be implemented in a file in the sibling constraints-indexes folder. The table-data folder of the main camdecmpsmd folder should exist to populate the camdecmpsmd.certification_statement table.

--- camdecmpsmd/tables/certification_statement.sql
+++ camdecmpsmd/tables/certification_statement.sql
@@ -13,16 +13,10 @@ COMMENT ON COLUMN camdecmpsmd.certification_statement.statement_id
     IS 'Certification Statement Ids';
 
 COMMENT ON COLUMN camdecmpsmd.certification_statement.prg_cd
-    IS 'Program code values.';
+    IS 'Unique program code for program specific certification statements.';
 
 COMMENT ON COLUMN camdecmpsmd.certification_statement.statement_location
     IS 'Path to certification statement files in the EASEY Content.';
 
 COMMENT ON COLUMN camdecmpsmd.certification_statement.display_order
     IS 'Order using statement_id.';
-
-CREATE UNIQUE INDEX prg_cd_unique ON camdecmpsmd.certification_statement (prg_cd)
-WHERE prg_cd IS NOT NULL;
-
-ALTER TABLE IF EXISTS camdecmpsmd.certification_statement
-    ADD CONSTRAINT pk_certification_statement PRIMARY KEY (statement_id);
