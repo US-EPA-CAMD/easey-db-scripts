8741fe865facc96c877bbf17f3ab79f8ce69746a
mark-hayward-erg <46684225+mark-hayward-erg@users.noreply.github.com>
2025-08-05 12:43:14 -0400

    Update delete_workspace.sql
    remove deletion of camdecmpsaux evaluation and submission log data

--- camdecmpswks/procedures/delete_workspace.sql
+++ camdecmpswks/procedures/delete_workspace.sql
@@ -7,10 +7,6 @@ CREATE OR REPLACE PROCEDURE camdecmpswks.delete_workspace(
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
-	TRUNCATE TABLE camdecmpswks.check_session CASCADE;
-	TRUNCATE TABLE camdecmpsaux.evaluation_set CASCADE;
-  TRUNCATE TABLE camdecmpsaux.submission_set CASCADE;
-
 	CALL camdecmpswks.delete_emissions_workspace();
 	CALL camdecmpswks.delete_qa_workspace();
 	CALL camdecmpswks.delete_monitor_plan_workspace();
