65be2716c64e21b50f175dcef1596a84c8ec3f4c
Mark Hayward <mark.hayward@erg.com>
2025-08-14 11:03:13 -0400

    repurpose camdecmps/procedures/initialize_em_submission_access.sql as a misc script (used to recreate em windows for a specified year and quarter) update init_and_close_em_submission_access.sql to use v_sysdate_as_date (not current_date) when setting window dates (procedure can be executed to act like any date) remove camdecmpswks.get_mp_location_list from drop_functions.sql (function is needed and used directly by APIs)

--- deployments/ecmps/release-v2.0-fix/Sprint18/6565/drop_functions.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint18/6565/drop_functions.sql
@@ -1,5 +1,3 @@
-drop function if exists camdecmpswks.get_mp_location_list(varchar);
-
 drop function if exists camdecmpsaux.get_em_submission_status(varchar, numeric, numeric);
 drop function if exists camdecmpsaux.get_last_submission(varchar, numeric, varchar, varchar, varchar, varchar);
 drop function if exists camdecmpsaux.get_last_submission(varchar, numeric, varchar);
@@ -12,3 +10,4 @@ drop function if exists camdecmps.get_last_em_submission(varchar, numeric, date,
 drop function if exists camdecmps.get_mp_location_list(varchar);
 drop procedure if exists camdecmps.init_and_close_em_submission_access(inout text, inout text);
 drop procedure if exists camdecmps.init_and_close_em_submission_access(in date, in numeric, inout text, inout text);
+drop procedure if exists camdecmps.initialize_em_submission_access(IN numeric, IN numeric, INOUT text, INOUT text);
