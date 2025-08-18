351a82739dcd6d0e17575737e7bb3e6e422adc28
Jhernandez29 <jonashernandez@erg.com>
2025-08-18 13:46:42 -0500

    Updated files to return array instead of mon plan id. Move file to sprint 24.

--- camdecmpswks/functions/get_plan_id_by_comment_begin_and_end_date.sql
+++ camdecmpswks/functions/get_plan_id_by_comment_begin_and_end_date.sql
@@ -1,29 +1,25 @@
 CREATE OR REPLACE FUNCTION camdecmpswks.get_plan_by_comment_begin_and_end_date(
-	locationid character varying,
+	locationids character varying[],
     comment_begin_date date,
 	comment_end_date date
     )
-    RETURNS character varying
+    RETURNS TABLE (mon_plan_id character varying)
     LANGUAGE 'plpgsql'
 
     COST 100
     VOLATILE 
     
 AS $BODY$
-DECLARE
-	identifier character varying;
 BEGIN 
-	SELECT
-    mp.mon_plan_id INTO identifier
+	RETURN QUERY SELECT DISTINCT
+        mp.mon_plan_id
     FROM
     camdecmpswks.monitor_plan mp
     JOIN camdecmpswks.monitor_plan_location mpl USING (mon_plan_id)
     JOIN camdecmpsmd.reporting_period brp ON mp.begin_rpt_period_id = brp.rpt_period_id
     LEFT JOIN camdecmpsmd.reporting_period erp ON mp.end_rpt_period_id = erp.rpt_period_id
 WHERE
-    mpl.mon_loc_id = ANY(ARRAY[locationid])
-    AND daterange(brp.begin_date, erp.end_date) && daterange(comment_begin_date, comment_end_date);
-	
-	return identifier;
+    mpl.mon_loc_id = ANY(locationids)
+    AND daterange(brp.begin_date, erp.end_date, '[]') && daterange(comment_begin_date, comment_end_date, '[]');
 END
 $BODY$;
