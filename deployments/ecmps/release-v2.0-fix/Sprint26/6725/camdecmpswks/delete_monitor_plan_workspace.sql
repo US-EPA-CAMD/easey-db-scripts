0b8915bfcd625f2215868b2f1db678d1378fc9b0
renaemyers <renaemyers@users.noreply.github.com>
2025-08-22 16:20:33 -0400

    Create delete_monitor_plan_workspace.sql
    Add truncate from Units table

---
+++ deployments/ecmps/release-v2.0-fix/Sprint26/6725/camdecmpswks/delete_monitor_plan_workspace.sql
@@ -0,0 +1,19 @@
+-- PROCEDURE: camdecmpswks.delete_monitor_plan_workspace()
+
+DROP PROCEDURE IF EXISTS camdecmpswks.delete_monitor_plan_workspace();
+
+CREATE OR REPLACE PROCEDURE camdecmpswks.delete_monitor_plan_workspace()
+ LANGUAGE plpgsql
+AS $procedure$
+BEGIN
+	TRUNCATE TABLE camdecmpswks.monitor_location CASCADE;
+	TRUNCATE TABLE camdecmpswks.monitor_plan CASCADE;
+	TRUNCATE TABLE camdecmpswks.unit_capacity CASCADE;
+	TRUNCATE TABLE camdecmpswks.unit_control CASCADE;
+	TRUNCATE TABLE camdecmpswks.unit_fuel CASCADE;
+	TRUNCATE TABLE camdecmpswks.unit_stack_configuration CASCADE;
+	TRUNCATE TABLE camdecmpswks.stack_pipe CASCADE;
+	TRUNCATE TABLE camdecmpswks.unit CASCADE;
+END;
+$procedure$
+;
