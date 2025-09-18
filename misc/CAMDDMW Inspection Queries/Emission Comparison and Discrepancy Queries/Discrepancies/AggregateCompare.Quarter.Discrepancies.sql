5d79dfa834ebb6998f8d09b2e4d6fa0c3ed368e7
DWhitten <Dwayne.Whitten@erg.com>
2025-09-18 14:53:52 -0400

    Addition of CAMPD queries to compare aggregate emissions data.

---
+++ misc/CAMDDMW Inspection Queries/Emission Comparison and Discrepancy Queries/Discrepancies/AggregateCompare.Quarter.Discrepancies.sql
@@ -0,0 +1,149 @@
+select  uni.oris_code,
+        uni.facility_name,
+        uni.unit_name,
+        uni.quarter,
+        string_agg( uni.Period, ', ' order by uni.Ord ) as Periods,
+        uni.op_time,
+        uni.op_hours,
+        uni.gload,
+        uni.sload,
+        uni.tload,
+        uni.hit,
+        uni.so2m,
+        uni.so2r,
+        uni.co2m,
+        uni.co2r,
+        uni.noxm,
+        uni.noxr
+  from  (
+            select  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid as unit_name,
+                    'Hour' Period, 1 Ord,
+                    prd.period_abbreviation as quarter,
+                    sum(dat.op_time) op_time,
+                    sum(ceil(dat.op_time)) op_hours,
+                    sum(dat.gload * dat.op_time) gload,
+                    sum(dat.sload * dat.op_time) sload,
+                    sum(dat.tload * dat.op_time) tload,
+                    sum(dat.heat_input) hit,
+                    sum(dat.so2_mass) so2m,
+                    avg(dat.so2_rate) so2r,
+                    sum(dat.co2_mass) co2m,
+                    avg(dat.co2_rate) co2r,
+                    sum(dat.nox_mass) noxm,
+                    avg(dat.nox_rate) noxr
+              from  camddmw.HOUR_UNIT_DATA dat
+                    join camd.UNIT unt using ( unit_id ) 
+                    join camd.PLANT fac using ( fac_id )
+                    join camdecmpsmd.REPORTING_PERIOD prd using (rpt_period_id )
+             group
+                by  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid,
+                    prd.period_abbreviation
+            union   all
+            select  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid as unit_name,
+                    'Day' Period, 2 Ord,
+                    prd.period_abbreviation as quarter,
+                    sum(dat.sum_op_time) op_time,
+                    sum(dat.count_op_time) op_hours,
+                    sum(dat.gload) gload,
+                    sum(dat.sload) sload,
+                    sum(dat.tload) tload,
+                    sum(dat.heat_input) hit,
+                    sum(dat.so2_mass_lbs) so2m,
+                    case when sum(dat.so2_rate_count) > 0 then sum(dat.so2_rate_sum) / sum(dat.so2_rate_count) else null end so2r,
+                    sum(dat.co2_mass) co2m,
+                    case when sum(dat.co2_rate_count) > 0 then sum(dat.co2_rate_sum) / sum(dat.co2_rate_count) else null end co2r,
+                    sum(dat.nox_mass_lbs) noxm,
+                    case when sum(dat.nox_rate_count) > 0 then sum(dat.nox_rate_sum) / sum(dat.nox_rate_count) else null end noxr
+              from  camddmw.DAY_UNIT_DATA dat
+                    join camd.UNIT unt using ( unit_id ) 
+                    join camd.PLANT fac using ( fac_id )
+                    join camdecmpsmd.REPORTING_PERIOD prd using (rpt_period_id )
+             group
+                by  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid,
+                    prd.period_abbreviation
+            union   all
+            select  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid as unit_name,
+                    'Month' Period, 3 Ord,
+                    prd.period_abbreviation as quarter,
+                    sum(dat.sum_op_time) op_time,
+                    sum(dat.count_op_time) op_hours,
+                    sum(dat.gload) gload,
+                    sum(dat.sload) sload,
+                    sum(dat.tload) tload,
+                    sum(dat.heat_input) hit,
+                    sum(dat.so2_mass_lbs) so2m,
+                    case when sum(dat.so2_rate_count) > 0 then sum(dat.so2_rate_sum) / sum(dat.so2_rate_count) else null end so2r,
+                    sum(dat.co2_mass) co2m,
+                    case when sum(dat.co2_rate_count) > 0 then sum(dat.co2_rate_sum) / sum(dat.co2_rate_count) else null end co2r,
+                    sum(dat.nox_mass_lbs) noxm,
+                    case when sum(dat.nox_rate_count) > 0 then sum(dat.nox_rate_sum) / sum(dat.nox_rate_count) else null end noxr
+              from  camddmw.MONTH_UNIT_DATA dat
+                    join camd.UNIT unt using ( unit_id ) 
+                    join camd.PLANT fac using ( fac_id )
+                    join camdecmpsmd.REPORTING_PERIOD prd using (rpt_period_id )
+             group
+                by  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid,
+                    prd.period_abbreviation
+            union   all
+            select  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid as unit_name,
+                    'Quarter' Period, 4 Ord,
+                    prd.period_abbreviation as quarter,
+                    sum(dat.sum_op_time) op_time,
+                    sum(dat.count_op_time) op_hours,
+                    sum(dat.gload) gload,
+                    sum(dat.sload) sload,
+                    sum(dat.tload) tload,
+                    sum(dat.heat_input) hit,
+                    sum(dat.so2_mass_lbs) so2m,
+                    case when sum(dat.so2_rate_count) > 0 then sum(dat.so2_rate_sum) / sum(dat.so2_rate_count) else null end so2r,
+                    sum(dat.co2_mass) co2m,
+                    case when sum(dat.co2_rate_count) > 0 then sum(dat.co2_rate_sum) / sum(dat.co2_rate_count) else null end co2r,
+                    sum(dat.nox_mass_lbs) noxm,
+                    case when sum(dat.nox_rate_count) > 0 then sum(dat.nox_rate_sum) / sum(dat.nox_rate_count) else null end noxr
+              from  camddmw.QUARTER_UNIT_DATA dat
+                    join camd.UNIT unt using ( unit_id ) 
+                    join camd.PLANT fac using ( fac_id )
+                    join camdecmpsmd.REPORTING_PERIOD prd using (rpt_period_id )
+             group
+                by  fac.oris_code,
+                    fac.facility_name,
+                    unt.unitid,
+                    prd.period_abbreviation
+        ) uni
+ group
+    by  uni.oris_code,
+        uni.facility_name,
+        uni.unit_name,
+        uni.quarter,
+        uni.op_time,
+        uni.op_hours,
+        uni.gload,
+        uni.sload,
+        uni.tload,
+        uni.hit,
+        uni.so2m,
+        uni.so2r,
+        uni.co2m,
+        uni.co2r,
+        uni.noxm,
+        uni.noxr
+having  ( string_agg( uni.Period, ', ' order by uni.Ord ) != 'Hour, Day, Month, Quarter' )
+ order
+    by  uni.oris_code,
+        uni.facility_name,
+        uni.unit_name,
+        uni.quarter
