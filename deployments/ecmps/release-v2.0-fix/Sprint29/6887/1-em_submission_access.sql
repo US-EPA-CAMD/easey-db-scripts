b22a79beb47207337520fa48c98d47df2c50b0f8
Jon Maxwell Diebold <max.diebold@erg.com>
2025-09-26 20:53:44 -0400

    feat(6887): populate submission_id column using vw_last_submission_in_window

--- deployments/ecmps/release-v2.0-fix/Sprint29/6887/1-em_submission_access.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint29/6887/1-em_submission_access.sql
@@ -5,3 +5,8 @@ ALTER TABLE camdecmpsaux.em_submission_access
 
 COMMENT ON COLUMN camdecmpsaux.em_submission_access.submission_id
     IS 'Foreign key to the SUBMISSION_ID in the SUBMISSION_QUEUE table';
+
+UPDATE camdecmpsaux.em_submission_access esa
+SET submission_id = lsiw.submission_id
+FROM camdecmpsaux.vw_last_submission_in_window lsiw
+WHERE lsiw.em_sub_access_id = esa.em_sub_access_id;
