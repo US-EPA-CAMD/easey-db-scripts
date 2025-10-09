c5e02f75e754c11ce19801c0257173c06e91e74a
spetros-do <153679433+spetros-do@users.noreply.github.com>
2025-10-09 11:47:49 -0700

    #6922: move cleanup script to release-v2.0; schema=camdecmps; remove deploy include

--- deployments/ecmps/release-v2.0/deploy.sh
+++ deployments/ecmps/release-v2.0/deploy.sh
@@ -325,7 +325,7 @@ if [ $2 == "POST_DEPLOYMENT_CLEANUP" ]; then
   DROP TABLE IF EXISTS camdaux.missing_oris CASCADE;
   DROP TABLE IF EXISTS camdaux.sftp_failures CASCADE;
   DROP TABLE IF EXISTS camdaux.sftp_log CASCADE;
-  \i ./camdecmpswks-em-supp-data-cleanup.sql
+  \i ./em-supp-data-cleanup.sql
   \i ./update-mp-qa-em-check-session-ids.sql
   CALL camdecmpswks.load_workspace();
   "
