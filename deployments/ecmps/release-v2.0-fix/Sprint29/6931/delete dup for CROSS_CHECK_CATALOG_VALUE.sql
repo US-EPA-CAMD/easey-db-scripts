2ac706f12e88ed47775314a2f16bbd2d022126b1
Anna <anna.yang@erg.com>
2025-10-14 17:12:22 -0400

    #6931-Generic Cross Check Value Table: Add Unique Constraint

--- deployments/ecmps/release-v2.0-fix/Sprint29/6931/delete dup for CROSS_CHECK_CATALOG_VALUE.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint29/6931/delete dup for CROSS_CHECK_CATALOG_VALUE.sql
@@ -1,9 +1,13 @@
-delete from camdecmpsmd.CROSS_CHECK_CATALOG_VALUE
-where cross_chk_catalog_value_id in
-(select cross_chk_catalog_value_id
+-- to check the duplicates
+select cross_chk_catalog_value_id
 from (select min(cross_chk_catalog_value_id) cross_chk_catalog_value_id,
 cross_chk_catalog_id, value1, value2, value3
 from camdecmpsmd.CROSS_CHECK_CATALOG_VALUE 
  group  by  cross_chk_catalog_id,value1, value2,value3
 having  count( * ) > 1 
- order by cross_chk_catalog_id, value1, value2, value3) val);
+ order by cross_chk_catalog_id, value1, value2, value3) dal;
+ 
+ --delete current existing duplicates records 
+ delete from camdecmpsmd.CROSS_CHECK_CATALOG_VALUE
+	where cross_chk_catalog_value_id in (112, 534,537,1520,1417, 440, 156, 9320);
+
