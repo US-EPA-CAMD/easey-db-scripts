06d0e643dcfb60f66deb5aa0c195d11bec5204c4
mark-hayward-erg <46684225+mark-hayward-erg@users.noreply.github.com>
2026-03-11 16:54:34 -0400

    Update update_USC_end_rpt_period_id.sql
    update to use a single merge statement to update end_date

--- deployments/ecmps/release-v2.0-fix/Sprint24/6437/update_USC_end_rpt_period_id.sql
+++ deployments/ecmps/release-v2.0-fix/Sprint24/6437/update_USC_end_rpt_period_id.sql
@@ -1,26 +1,24 @@
---update retired end_rpt_period_id for table unit_stack_configuration
-
-DECLARE
-  cursor usc_cursor is  
-   select config_id,(select  min ( cmb.Retire_Date ) -1
-                     from  (
-					         select uos.Begin_Date as Retire_Date from UNIT_OP_STATUS uos 
-                              where uos.Op_Status_Cd = 'RET' and uos.End_Date is null 
-                               and  uos.unit_id= usc.unit_id
-                           union   all
-                            select stp.Retire_Date  from  STACK_PIPE stp   
-                              where stp.stack_pipe_id = usc.stack_pipe_id and stp.Retire_Date is not null
-                                ) cmb 
-				    ) as first_retire_date
-   from UNIT_STACK_CONFIGURATION usc
-    join stack_pipe sp on  sp.Stack_Pipe_Id = usc.Stack_Pipe_Id
-    join UNIT_OP_STATUS  uos on uos.unit_id=usc.unit_id and OP_STATUS_CD= 'RET' and uos.END_DATE is null
-      where usc.end_date is null; 
-
-BEGIN
-   FOR usc_record IN usc_cursor
-    LOOP   
-        update UNIT_STACK_CONFIGURATION set end_date = usc_record.first_retire_date
-            where unit_stack_configuration.config_id = usc_record.config_id;
-   END LOOP;
-END;
+/* Formatted on 3/11/2026 4:15:04 PM (QP5 v5.300) */
+MERGE INTO unit_stack_configuration d
+     USING (    SELECT usc.config_id,
+       usc.unit_id,
+       usc.stack_pipe_id,
+       usc.begin_date,
+       uos.begin_date AS unit_retire_date,
+       sp.retire_date AS stack_pipe_retire_date,
+       LEAST (uos.begin_date, NVL (sp.retire_date, uos.begin_date))
+           AS end_date
+  FROM unit_stack_configuration  usc
+       INNER JOIN unit_op_status uos
+           ON     usc.unit_id = uos.unit_id
+              AND uos.OP_STATUS_CD = 'OPR'
+              AND uos.end_date IS NULL
+       INNER JOIN stack_pipe sp ON usc.stack_pipe_id = sp.stack_pipe_id
+ WHERE usc.end_date IS NULL) s
+        ON (d.config_id = s.config_id)
+WHEN MATCHED
+THEN
+    UPDATE SET
+        d.USERID = 'ECMPS20',
+        d.UPDATE_DATE = SYSDATE,
+        d.END_DATE = s.end_date;
