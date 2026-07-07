483132579fa336dd2841715443f6ca3b984da94b
Mark Hayward <mark.hayward@erg.com>
2026-07-07 10:59:52 -0400

    new submission_queue indexes

--- camdecmpsaux/constraints-indexes/2-submission_queue.sql
+++ camdecmpsaux/constraints-indexes/2-submission_queue.sql
@@ -43,3 +43,15 @@ CREATE INDEX IF NOT EXISTS idx_submission_queue_severity_cd
 CREATE INDEX IF NOT EXISTS idx_submission_queue_status_cd
     ON camdecmpsaux.submission_queue USING btree
     (status_cd COLLATE pg_catalog."default" ASC NULLS LAST);
+	
+CREATE INDEX IF NOT EXISTS idx_submission_queue_completed_time 
+	ON camdecmpsaux.submission_queue USING btree 
+	(completed_time);
+
+CREATE INDEX IF NOT EXISTS idx_submission_queue_started_time 
+	ON camdecmpsaux.submission_queue USING btree 
+	(started_time);
+
+CREATE INDEX IF NOT EXISTS idx_submission_queue_queued_time 
+	ON camdecmpsaux.submission_queue
+	((queued_time::date));	
