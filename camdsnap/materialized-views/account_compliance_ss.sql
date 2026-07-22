ff51af1e8f8e46ed4e73b622244a680727babbea
DWhitten <Dwayne.Whitten@erg.com>
2026-07-22 16:08:25 -0400

    #7238 Addition of ACCOUNT_COMPLIANCE_SS (not tested).

---
+++ camdsnap/materialized-views/account_compliance_ss.sql
@@ -0,0 +1,18 @@
+create materialized view camdsnap.ACCOUNT_COMPLIANCE_SS
+as 
+select  account_comp_id,
+        account_id,
+        add_date,
+        comp_period_id,
+        comp_status_cd,
+        credit_total,
+        emiss_liability,
+        offset_liability,
+        takeback_liability,
+        underutilization_liability,
+        update_date,
+        userid
+  from  camdams.ACCOUNT_COMPLIANCE;
+
+
+comment on materialized view camdsnap.ACCOUNT_COMPLIANCE_SS is 'snapshot table for snapshot CAMDSNAP.ACCOUNT_COMPLIANCE_SS';
