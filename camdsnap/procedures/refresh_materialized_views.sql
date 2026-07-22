955d22b51c6313ad9ae44b771ea056d7a97a029f
DWhitten <Dwayne.Whitten@erg.com>
2026-07-22 18:07:42 -0400

    #7238 Addition of NOX_COMP_PLAN_RESULT_SS (not tested).

--- camdsnap/procedures/refresh_materialized_views.sql
+++ camdsnap/procedures/refresh_materialized_views.sql
@@ -23,6 +23,7 @@ begin
     -- refresh materialized view camdsnap.COMPLIANCE_EMISSION_SS;
     -- refresh materialized view camdsnap.COMPLIANCE_PERIOD_SS;
     -- refresh materialized view camdsnap.COMPLIANCE_PROBLEM_TRANS_SS;
+    -- refresh materialized view camdsnap.NOX_COMP_PLAN_RESULT_SS;
     refresh materialized view camdsnap.FACILITY_SS;
     refresh materialized view camdsnap.FACILITY_PEOPLE_SS;
     refresh materialized view camdsnap.MONITOR_LOCATION_SS;
