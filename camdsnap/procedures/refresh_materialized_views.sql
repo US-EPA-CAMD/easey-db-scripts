7aeb5e9da5ad9a6cb9123803fa8867bbdc27f056
DWhitten <Dwayne.Whitten@erg.com>
2026-07-22 16:25:52 -0400

    #7238 Addition of ACCOUNT_FACILITY_SS and ACCOUNT_OWNER_SS (not tested).

--- camdsnap/procedures/refresh_materialized_views.sql
+++ camdsnap/procedures/refresh_materialized_views.sql
@@ -13,6 +13,8 @@ begin
     -- refresh materialized view camdsnap.ACCOUNT_COMPLIANCE_LIAB_SS;
     -- refresh materialized view camdsnap.ACCOUNT_COMPLIANCE_PENALTY;
     -- refresh materialized view camdsnap.ACCOUNT_COMPLIANCE_SS;
+    -- refresh materialized view camdsnap.ACCOUNT_FACILITY_SS;
+    -- refresh materialized view camdsnap.ACCOUNT_OWNER_SS;
     refresh materialized view camdsnap.FACILITY_SS;
     refresh materialized view camdsnap.FACILITY_PEOPLE_SS;
     refresh materialized view camdsnap.MONITOR_LOCATION_SS;
