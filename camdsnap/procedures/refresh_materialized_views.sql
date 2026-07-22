e8e050b9af2f9bc5a1f74609327c7ef861f5a384
DWhitten <Dwayne.Whitten@erg.com>
2026-07-21 21:48:27 -0400

    #7238 Addition of MONITOR_PLAN_REPORTING_FREQ_SS.

--- camdsnap/procedures/refresh_materialized_views.sql
+++ camdsnap/procedures/refresh_materialized_views.sql
@@ -8,6 +8,7 @@ begin
     refresh materialized view camdsnap.FACILITY_PEOPLE_SS;
     refresh materialized view camdsnap.MONITOR_LOCATION_SS;
     refresh materialized view camdsnap.MONITOR_PLAN_LOCATION_SS;
+    refresh materialized view camdsnap.MONITOR_PLAN_REPORTING_FREQ_SS;
     
 end;
 $$;
