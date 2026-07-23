32cbe3a1cd1475a7eb80796c74d7a59853864d30
DWhitten <Dwayne.Whitten@erg.com>
2026-07-22 23:05:02 -0400

    #7238 Addition of UNIT_PROGRAM_PHASE_SS (not tested).

--- camdsnap/procedures/refresh_materialized_views.sql
+++ camdsnap/procedures/refresh_materialized_views.sql
@@ -51,6 +51,7 @@ begin
     -- refresh materialized view camdsnap.UNIT_OWNER_SS;
     -- refresh materialized view camdsnap.UNIT_PHYSICAL_MOVE_SS;
     refresh materialized view camdsnap.UNIT_PROGRAM_EXEMPTION_SS;
+    -- refresh materialized view camdsnap.UNIT_PROGRAM_PHASE_SS;
 
     ----------------------------------------------
     -- First Level Materialized View Dependency --
