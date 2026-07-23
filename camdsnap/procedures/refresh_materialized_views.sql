448a2907b153d3ee16bf67775c2397c8d4ae3ebb
DWhitten <Dwayne.Whitten@erg.com>
2026-07-22 22:32:43 -0400

    #7238 Addition of TRANSACTION_SS (not tested).

--- camdsnap/procedures/refresh_materialized_views.sql
+++ camdsnap/procedures/refresh_materialized_views.sql
@@ -40,6 +40,7 @@ begin
     -- refresh materialized view camdsnap.STAFF_AGENCY_SS;
     -- refresh materialized view camdsnap.TCOMPPROB_TRANSACT_SS;
     -- refresh materialized view camdsnap.TRANSACTION_BLOCK_SS;
+    -- refresh materialized view camdsnap.TRANSACTION_SS;
     refresh materialized view camdsnap.UNIT_BT_TYPE_SS;
     refresh materialized view camdsnap.UNIT_CAPACITY_SS;
     refresh materialized view camdsnap.UNIT_CONTROL_SS;
