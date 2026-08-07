8361c8792c7bb4c6948bef3c511194bf0da419ed
DWhitten <Dwayne.Whitten@erg.com>
2026-08-07 17:55:24 -0400

    #7238 Removed PII columns from PEOPLE_SS and PEOPLE_DETAIL_SS materialized views.

--- camdsnap/materialized-views/people_detail_ss.sql
+++ camdsnap/materialized-views/people_detail_ss.sql
@@ -1,58 +1,13 @@
 create materialized view camdsnap.PEOPLE_DETAIL_SS
 as 
 select  ppl_id,
-        initial_password_ind as initial_psswrd_flg,
-        password,
-        coalesce( ppl.password_change_date, ppl.add_date ) as password_chg_date,
-        to_char( coalesce( ppl.password_change_date, ppl.add_date ), 'HH:MI:SS PM' ) as password_chg_time,
         last_name || ', ' || first_name || ' ' || middle_initial as name,
         first_name || ' ' || ltrim( coalesce( middle_initial || ' ', '' ) ) || last_name || case when suffix is not null then ' ' || suffix else '' end as display_name,
         last_name,
         first_name,
         middle_initial,
         suffix,
-        job_title,
-        cmp.company_name as affiliation,
-        address1,
-        address2,
-        city,
-        state_cd as state,
-        zip_code,
-        case
-            when ppl.zip_code is null
-            then null
-            when length( ppl.zip_code ) > 5
-            then substr( ppl.zip_code, 1, 5 ) || '-' || substr( ppl.zip_code, 6, 10 )
-            else ppl.zip_code
-        end as formatted_zip_code,
-        country_name as country_cd,
-        login,
-        email_address,
-        fax_number,
-        case
-            when ppl.fax_number is null
-            then null
-            when length( ppl.fax_number ) = 9
-            then substr( ppl.fax_number, 1, 3 ) || '-' || substr( ppl.fax_number, 4, 3 )
-            else '(' || substr( ppl.fax_number, 1, 3 ) || ') ' || substr( ppl.fax_number, 4, 3 ) || '-' || substr( ppl.fax_number, 7, 4 )
-        end as formatted_fax_number,
-        phone_number,
-        extension,
-        case
-            when ppl.phone_number is null
-            then null
-            when length( ppl.phone_number ) = 9
-            then case
-                    when length( ppl.extension ) > 1
-                    then substr( ppl.phone_number, 1, 3 ) || '-' || substr( ppl.phone_number, 4, 3 )
-                    else substr( ppl.phone_number, 1, 3 ) || '-' || substr( ppl.phone_number, 4, 3 ) || ' x ' || ppl.extension
-                 end
-            else case
-                    when length( ppl.extension ) > 1
-                    then '(' || substr( ppl.phone_number, 1, 3 ) || ') ' || substr( ppl.phone_number, 4, 3 ) || '-' || substr( ppl.phone_number, 7, 4 )
-                    else '(' || substr( ppl.phone_number, 1, 3 ) || ') ' || substr( ppl.phone_number, 4, 3 ) || '-' || substr( ppl.phone_number, 7, 4 ) || ' x ' || ppl.extension
-                 end
-        end as formatted_phone_number,
+        cmp.company_name as affiliation
         case when ppl.PERSON_TYPE_CD = 'IND' then ppl.ppl_id else null end as rep_id,
         ppl.userid,
         ppl.update_date,
@@ -63,7 +18,6 @@ select  ppl_id,
         ptc.person_type_group_cd,
         ( ppl.person_type_cd || ', ' || ptc.person_type_group_cd ) as person_type_and_grp,
         to_char( cmp.company_id ) as comp_id,
-        ppl.ppl_comment,
         now() as refresh_time
   from  camd.PERSON ppl
         join camdmd.COUNTRY_CODE ccd
