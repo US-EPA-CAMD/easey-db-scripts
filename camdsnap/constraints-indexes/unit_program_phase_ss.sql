83fd20a22609166d5145b58fc39699b7f4bdab89
mark-hayward-erg <46684225+mark-hayward-erg@users.noreply.github.com>
2026-09-02 10:50:39 -0400

    Update unit_program_phase_ss.sql

--- camdsnap/constraints-indexes/unit_program_phase_ss.sql
+++ camdsnap/constraints-indexes/unit_program_phase_ss.sql
@@ -3,3 +3,5 @@
 --------------------
 
 create unique index UNIT_PROGRAM_PHASE_SS_UQ on camdsnap.UNIT_PROGRAM_PHASE_SS ( up_id, begin_year );
+
+CREATE INDEX unit_program_phase_ss_phase_idx ON camdsnap.unit_program_phase_ss USING btree (phase);
